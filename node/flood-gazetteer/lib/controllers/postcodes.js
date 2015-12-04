ae8b09e6b9b4cec2ffafb537e51025b488be6c90
Tedd <tjmason.ea@gmail.com>
2015-12-04 08:20:47 +0000

    Gazetteer work to include updated address data

--- node/flood-gazetteer/lib/controllers/postcodes.js
+++ node/flood-gazetteer/lib/controllers/postcodes.js
@@ -1,19 +1,19 @@
 var Boom = require('boom')
-var Address = require('../models/address')
+var Postcode = require('../models/postcode')
 
 module.exports = function postcodes (request, reply) {
   var db = request.server.plugins['hapi-mongodb'].db
   var regexp = new RegExp('^' + request.params.partialPostcode.toUpperCase().replace(' ', ''))
 
-  db.collection('address')
-    .distinct('POSTCODE', { 'PC_NOSPACE': regexp }, function (err, docs) {
+  db.collection('postcode')
+    .find({ 'PC_NOSPACE': regexp }).toArray(function (err, docs) {
       if (err) {
         request.log('error', err)
         return reply(Boom.badRequest())
       }
 
       reply(docs.map(function (doc) {
-        return new Address(doc)
+        return new Postcode(doc)
       }))
     })
 }
