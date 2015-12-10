acde046240afde5cfe49deb5263e662f23e948d0
Dave Stone <dstone1.ea@gmail.com>
2015-12-10 13:57:32 +0000

    Updated service response

--- node/flood-risk-service/lib/controllers/risk.js
+++ node/flood-risk-service/lib/controllers/risk.js
@@ -6,6 +6,9 @@ module.exports = function getFloodRisk (request, reply) {
   var params = request.params
 
   service.calculateFloodRisk(db, params.x, params.y, params.radius, function (err, result) {
+    /*
+     * Do some assertions around the result we get back from the database
+     */
     if (err) {
       request.log('error', err)
       return reply(Boom.badRequest('Database call failed'))
@@ -16,18 +19,49 @@ module.exports = function getFloodRisk (request, reply) {
       return reply(Boom.badRequest('Invalid result'))
     }
 
-    var response = result.rows.map(function (item) {
-      var risk = item.calculate_flood_risk
-      return {
-        inFloodAlertArea: risk.in_flood_alert_area,
-        inFloodWarningArea: risk.in_flood_warning_area,
-        leadLocalFloodAuthority: risk.lead_local_flood_authority,
-        reservoirRisk: risk.reservoir_risk,
-        riversAndSeasRisk: risk.rofrs_risk,
-        surfaceWaterRisk: risk.surface_water_risk,
-        surfaceWaterSuitability: risk.surface_water_suitability
+    var risk = result.rows[0].calculate_flood_risk
+
+    if (!risk) {
+      request.log('error', new Error('Invalid result - missing calculate_flood_risk key'))
+      return reply(Boom.badRequest('Invalid result - missing calculate_flood_risk key'))
+    }
+
+    /*
+     * If we get here we can be sure we have a valid result from
+     * the database and we can start to prepare our return response
+     */
+    var reservoirRisk = null
+    var riverAndSeaRisk = null
+
+    if (risk.reservoir_risk) {
+      reservoirRisk = {
+        reservoirName: risk.reservoir_risk.resname,
+        location: risk.reservoir_risk.location,
+        riskDesignation: risk.reservoir_risk.risk_desig,
+        isUtilityCompany: risk.reservoir_risk.utcompany,
+        leadLocalFloodAuthority: risk.reservoir_risk.llfa_name,
+        environmentAgencyArea: risk.reservoir_risk.ea_area,
+        comments: risk.reservoir_risk.comments
       }
-    })[0]
+    }
+
+    if (risk.rofrs_risk) {
+      riverAndSeaRisk = {
+        probabilityForBand: risk.rofrs_risk.prob_4band,
+        suitability: risk.rofrs_risk.suitability,
+        riskForInsuranceSOP: risk.rofrs_risk.risk_for_insurance_sop
+      }
+    }
+
+    var response = {
+      inFloodAlertArea: risk.in_flood_alert_area,
+      inFloodWarningArea: risk.in_flood_warning_area,
+      leadLocalFloodAuthority: risk.lead_local_flood_authority,
+      reservoirRisk: reservoirRisk,
+      riverAndSeaRisk: riverAndSeaRisk,
+      surfaceWaterRisk: risk.surface_water_risk,
+      surfaceWaterSuitability: risk.surface_water_suitability
+    }
 
     reply(response)
   })
