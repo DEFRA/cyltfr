c033ce1dc0b01627aa06093c8d3e9c132368de02
Paul Shaw <pmshaw.ea@gmail.com>
2016-03-09 09:32:25 +0000

    scripts to correct geometry errors in ltfri spatial data

---
+++ spatial-data-loader/scripts/data-quality/ltfri-drop-tables-ddl.sql
@@ -0,0 +1,95 @@
+\echo 'Extra Info - Dropping original table'
+
+DROP TABLE u_ltfri.extra_info_bv_bng;
+
+\echo 'Flood Alert Areas - Dropping original table'
+
+DROP TABLE u_ltfri.flood_alert_area_bv_bng;
+
+\echo 'Flood Warning Areas - Dropping original table'
+
+DROP TABLE u_ltfri.flood_warning_area_bv_bng;
+
+\echo 'Lead Local Flood Authorities - Dropping original table'
+
+DROP TABLE u_ltfri.lead_local_flood_authority_bv_bng;
+
+\echo 'River Level Depth and Flow Gauges - Dropping original table'
+
+DROP TABLE u_ltfri.river_level_depth_and_flow_gauges_bv_bng;
+
+\echo 'ROF Reservoir Depth - Dropping original table'
+
+DROP TABLE u_ltfri.rof_reservoir_depth_bv_bng;
+
+\echo 'ROF Reservoir Extent - Dropping original table'
+
+DROP TABLE u_ltfri.rof_reservoir_extent_bv_bng;
+
+\echo 'ROF Reservoir Extent Merged - Dropping original table'
+
+DROP TABLE u_ltfri.rof_reservoir_extent_merged_bv_bng;
+
+\echo 'ROF Reservoir Velocity - Dropping original table'
+
+DROP TABLE u_ltfri.rof_reservoir_velocity_bv_bng;
+
+\echo 'ROFRS England - Dropping original table'
+
+DROP TABLE u_ltfri.rofrs_england_bv_bng;
+
+\echo 'Sea Level Depth Gauges - Dropping original table'
+
+DROP TABLE u_ltfri.sea_level_depth_gauges_bv_bng;
+
+\echo 'UFMFSW Depth 1 in 1000 - Dropping original table'
+
+DROP TABLE u_ltfri.ufmfsw_depth_1_in_1000_bv_bng;
+
+\echo 'UFMFSW Depth 1 in 100 - Dropping original table'
+
+DROP TABLE u_ltfri.ufmfsw_depth_1_in_100_bv_bng;
+
+\echo 'UFMFSW Depth 1 in 30 - Dropping original table'
+
+DROP TABLE u_ltfri.ufmfsw_depth_1_in_30_bv_bng;
+
+\echo 'UFMFSW Extent 1 in 1000 - Dropping original table'
+
+DROP TABLE u_ltfri.ufmfsw_extent_1_in_1000_bv_bng;
+
+\echo 'UFMFSW Extent 1 in 100 - Dropping original table'
+
+DROP TABLE u_ltfri.ufmfsw_extent_1_in_100_bv_bng;
+
+\echo 'UFMFSW Extent 1 in 30 - Dropping original table'
+
+DROP TABLE u_ltfri.ufmfsw_extent_1_in_30_bv_bng;
+
+\echo 'UFMFSW Flow Direction 1 in 1000 - Dropping original table'
+
+DROP TABLE u_ltfri.ufmfsw_flow_direction_1_in_1000_bv_bng;
+
+\echo 'UFMFSW Flow Direction 1 in 100 - Dropping original table'
+
+DROP TABLE u_ltfri.ufmfsw_flow_direction_1_in_100_bv_bng;
+
+\echo 'UFMFSW Flow Direction 1 in 30 - Dropping original table'
+
+DROP TABLE u_ltfri.ufmfsw_flow_direction_1_in_30_bv_bng;
+
+\echo 'UFMFSW Suitability - Dropping original table'
+
+DROP TABLE u_ltfri.ufmfsw_suitability_bv_bng;
+
+\echo 'UFMFSW Velocity 1 in 1000 - Dropping original table'
+
+DROP TABLE u_ltfri.ufmfsw_velocity_1_in_1000_bv_bng;
+
+\echo 'UFMFSW Velocity 1 in 100 - Dropping original table'
+
+DROP TABLE u_ltfri.ufmfsw_velocity_1_in_100_bv_bng;
+
+\echo 'UFMFSW Velocity 1 in 30 - Dropping original table'
+
+DROP TABLE u_ltfri.ufmfsw_velocity_1_in_30_bv_bng;
---
+++ spatial-data-loader/scripts/data-quality/ltfri-rename-valid-tables-ddl.sql
@@ -0,0 +1,96 @@
+
+\echo 'Extra Info - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.extra_info_bv_bng_valid RENAME TO extra_info_bv_bng;
+
+\echo 'Flood Alert Areas - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.flood_alert_area_bv_bng_valid RENAME TO flood_alert_area_bv_bng;
+
+\echo 'Flood Warning Areas - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.flood_warning_area_bv_bng_valid RENAME TO flood_warning_area_bv_bng;
+
+\echo 'Lead Local Flood Authorities - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.lead_local_flood_authority_bv_bng_valid RENAME TO lead_local_flood_authority_bv_bng;
+
+\echo 'River Level Depth and Flow Gauges - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.river_level_depth_and_flow_gauges_bv_bng_valid RENAME TO river_level_depth_and_flow_gauges_bv_bng;
+
+\echo 'ROF Reservoir Depth - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.rof_reservoir_depth_bv_bng_valid RENAME TO rof_reservoir_depth_bv_bng;
+
+\echo 'ROF Reservoir Extent - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.rof_reservoir_extent_bv_bng_valid RENAME TO rof_reservoir_extent_bv_bng;
+
+\echo 'ROF Reservoir Extent Merged - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.rof_reservoir_extent_merged_bv_bng_valid RENAME TO rof_reservoir_extent_merged_bv_bng;
+
+\echo 'ROF Reservoir Velocity - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.rof_reservoir_velocity_bv_bng_valid RENAME TO rof_reservoir_velocity_bv_bng;
+
+\echo 'ROFRS England - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.rofrs_england_bv_bng_valid RENAME TO rofrs_england_bv_bng;
+
+\echo 'Sea Level Depth Gauges - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.sea_level_depth_gauges_bv_bng_valid RENAME TO sea_level_depth_gauges_bv_bng;
+
+\echo 'UFMFSW Depth 1 in 1000 - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.ufmfsw_depth_1_in_1000_bv_bng_valid RENAME TO ufmfsw_depth_1_in_1000_bv_bng;
+
+\echo 'UFMFSW Depth 1 in 100 - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.ufmfsw_depth_1_in_100_bv_bng_valid RENAME TO ufmfsw_depth_1_in_100_bv_bng;
+
+\echo 'UFMFSW Depth 1 in 30 - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.ufmfsw_depth_1_in_30_bv_bng_valid RENAME TO ufmfsw_depth_1_in_30_bv_bng;
+
+\echo 'UFMFSW Extent 1 in 1000 - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.ufmfsw_extent_1_in_1000_bv_bng_valid RENAME TO ufmfsw_extent_1_in_1000_bv_bng;
+
+\echo 'UFMFSW Extent 1 in 100 - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.ufmfsw_extent_1_in_100_bv_bng_valid RENAME TO ufmfsw_extent_1_in_100_bv_bng;
+
+\echo 'UFMFSW Extent 1 in 30 - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.ufmfsw_extent_1_in_30_bv_bng_valid RENAME TO ufmfsw_extent_1_in_30_bv_bng;
+
+\echo 'UFMFSW Flow Direction 1 in 1000 - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.ufmfsw_flow_direction_1_in_1000_bv_bng_valid RENAME TO ufmfsw_flow_direction_1_in_1000_bv_bng;
+
+\echo 'UFMFSW Flow Direction 1 in 100 - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.ufmfsw_flow_direction_1_in_100_bv_bng_valid RENAME TO ufmfsw_flow_direction_1_in_100_bv_bng;
+
+\echo 'UFMFSW Flow Direction 1 in 30 - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.ufmfsw_flow_direction_1_in_30_bv_bng_valid RENAME TO ufmfsw_flow_direction_1_in_30_bv_bng;
+
+\echo 'UFMFSW Suitability - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.ufmfsw_suitability_bv_bng_valid RENAME TO ufmfsw_suitability_bv_bng;
+
+\echo 'UFMFSW Velocity 1 in 1000 - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.ufmfsw_velocity_1_in_1000_bv_bng_valid RENAME TO ufmfsw_velocity_1_in_1000_bv_bng;
+
+\echo 'UFMFSW Velocity 1 in 100 - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.ufmfsw_velocity_1_in_100_bv_bng_valid RENAME TO ufmfsw_velocity_1_in_100_bv_bng;
+
+\echo 'UFMFSW Velocity 1 in 30 - Renaming _valid table original table'
+
+ALTER TABLE u_ltfri.ufmfsw_velocity_1_in_30_bv_bng_valid RENAME TO ufmfsw_velocity_1_in_30_bv_bng;
