f8c1682cd1d3749bfa1b22f15f160e9fe8d41f96
Tedd <tjmason.ea@gmail.com>
2016-04-26 13:09:40 +0100

    map tests

--- node/risk-app-tests/common/map.js
+++ node/risk-app-tests/common/map.js
@@ -1,5 +1,5 @@
 module.exports = {
-  loadPageNoParams: function (mapPage) {
+  loadPageNoParams: function (mapPage, callback) {
     mapPage.navigate()
       .assert.title('Long Term Flood Risk Information - GOV.UK')
       .assert.containsText('@heading', 'Learn more about this area\'s flood risk')
@@ -15,26 +15,63 @@ module.exports = {
   assertRiskAddressVisible: function (mapPage) {
     mapPage.assert.elementPresent('@riskAddressLink')
   },
-  assertMapParentSelected: function (mapPage, mapType) {
+  assertMapSelected: function (mapPage, mapType) {
     // selection relies on entire page to have loaded so wait
     mapPage.waitForElementPresent('.selected', 5000)
       .assert.cssClassPresent('@' + mapType, 'selected')
   },
-  assertMapParentNotSelected: function (mapPage, mapType) {
+  assertMapNotSelected: function (mapPage, mapType) {
     // selection relies on entire page to have loaded so wait
     mapPage.waitForElementPresent('.selected', 5000)
       .assert.cssClassNotPresent('@' + mapType, 'selected')
   },
-  assertLayerSelected: function (mapPage, layer) {
-    // not implemented
-  },
-  assertLayerNotSelected: function (mapPage, layer) {
-    // not implemented
-  },
   assertIsDetailedView: function (mapPage) {
     mapPage.assert.cssClassPresent('@mapContainer', 'detailed')
+    // Rivers and sea
+    mapPage.assert.visible('@RiversOrSea_1-ROFRS')
+    mapPage.assert.visible('@RiversOrSea_2-FWLRSF')
+
+    // Surface water
+    mapPage.assert.visible('@SurfaceWater_6-SW-Extent')
+    mapPage.assert.visible('@SurfaceWater_9-SWDH')
+    mapPage.assert.visible('@SurfaceWater_12-SWVH')
+    mapPage.assert.visible('@SurfaceWater_8-SWDM')
+    mapPage.assert.visible('@SurfaceWater_11-SWVM')
+    mapPage.assert.visible('@SurfaceWater_7-SWDL')
+    mapPage.assert.visible('@SurfaceWater_10-SWVL')
+
+    // reservoirs
+    mapPage.assert.visible('@Reservoirs_3-ROFR')
+    mapPage.assert.visible('@Reservoirs_4-DOFR')
+    mapPage.assert.visible('@Reservoirs_5-SOFR')
   },
   assertIsBasicView: function (mapPage) {
     mapPage.assert.cssClassNotPresent('@mapContainer', 'detailed')
+
+    // Rivers and sea
+    mapPage.assert.visible('@RiversOrSea_1-ROFRS')
+    mapPage.assert.hidden('@RiversOrSea_2-FWLRSF')
+
+    // Surface water
+    mapPage.assert.visible('@SurfaceWater_6-SW-Extent')
+    mapPage.assert.hidden('@SurfaceWater_9-SWDH')
+    mapPage.assert.hidden('@SurfaceWater_12-SWVH')
+    mapPage.assert.hidden('@SurfaceWater_8-SWDM')
+    mapPage.assert.hidden('@SurfaceWater_11-SWVM')
+    mapPage.assert.hidden('@SurfaceWater_7-SWDL')
+    mapPage.assert.hidden('@SurfaceWater_10-SWVL')
+
+    // reservoirs
+    mapPage.assert.visible('@Reservoirs_3-ROFR')
+    mapPage.assert.hidden('@Reservoirs_4-DOFR')
+    mapPage.assert.hidden('@Reservoirs_5-SOFR')
+  },
+  mobile: {
+    assertMapSelected: function (mapPage, mapType) {
+      console.log('not implemented!!')
+    },
+    assertMapNotSelected: function (mapPage, mapType) {
+      console.log('not implemented!!')
+    }
   }
 }
--- node/risk-app-tests/pages/map.js
+++ node/risk-app-tests/pages/map.js
@@ -8,9 +8,22 @@ module.exports = {
     'mapContainer': '#map-page .map-container',
     'riskAddressLink': '#map-page a[data-id="risk-address"]',
     'mapSwitch': '#map-page .map-switch a',
+    'mobileNav': '.visible-mobile select',
     'RiversOrSea': '#map-page li#RiversOrSea',
     'SurfaceWater': '#map-page li#SurfaceWater',
-    'Reservoirs': '#map-page li#Reservoirs'
+    'Reservoirs': '#map-page li#Reservoirs',
+    'RiversOrSea_1-ROFRS': '#map-page li#RiversOrSea_1-ROFRS',
+    'RiversOrSea_2-FWLRSF': '#map-page li#RiversOrSea_2-FWLRSF',
+    'SurfaceWater_6-SW-Extent': '#map-page li#SurfaceWater_6-SW-Extent',
+    'SurfaceWater_9-SWDH': '#map-page li#SurfaceWater_9-SWDH',
+    'SurfaceWater_12-SWVH': '#map-page li#SurfaceWater_12-SWVH',
+    'SurfaceWater_8-SWDM': '#map-page li#SurfaceWater_8-SWDM',
+    'SurfaceWater_11-SWVM': '#map-page li#SurfaceWater_11-SWVM',
+    'SurfaceWater_7-SWDL': '#map-page li#SurfaceWater_7-SWDL',
+    'SurfaceWater_10-SWVL': '#map-page li#SurfaceWater_10-SWVL',
+    'Reservoirs_3-ROFR': '#map-page li#Reservoirs_3-ROFR',
+    'Reservoirs_4-DOFR': '#map-page li#Reservoirs_4-DOFR',
+    'Reservoirs_5-SOFR': '#map-page li#Reservoirs_5-SOFR'
   },
   commands: [{
     loadPageWithParams: function (addressId, easting, northing, mapType) {
@@ -24,6 +37,17 @@ module.exports = {
   }, {
     selectMap: function (ref) {
       return this.waitForElementVisible('@' + ref, 2000).click('@' + ref)
+    }
+  }, {
+    selectMapMobile: function (ref) {
+      return this.waitForElementVisible('@mobileNav', 2000).click('select option[value="' + ref + '"]')
+    }
+  }, {
+    isMobile: function (callback) {
+      this.waitForElementVisible('button.ol-zoom-in', 20000)
+        .isVisible('@mobileNav', function (result) {
+          callback(result.value)
+        })
     }
   }]
 }
