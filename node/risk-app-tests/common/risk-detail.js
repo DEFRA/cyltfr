17809b8fafac85649f0fe3d372782dfb1445dca0
Tedd <tjmason.ea@gmail.com>
2016-08-05 11:32:23 +0100

    FLO-1518

--- node/risk-app-tests/common/risk-detail.js
+++ node/risk-app-tests/common/risk-detail.js
@@ -15,16 +15,16 @@ module.exports = {
       riskDetailPage.assert.containsText('@riverAndSeaPanel', 'The flood risk from rivers or the sea is ' + info.riverAndSeaRisk.toLowerCase())
       switch (info.riverAndSeaRisk) {
         case RiskLevel.High:
-          riskDetailPage.assert.containsText('@riverAndSeaPanel', 'High risk means that each year this area has a chance of flooding of greater than 3.3% (1 in 30).')
+          riskDetailPage.assert.containsText('@riverAndSeaPanel', 'High risk means that each year this area has a chance of flooding of greater than 3.3%.')
           break
         case RiskLevel.Medium:
-          riskDetailPage.assert.containsText('@riverAndSeaPanel', 'Medium risk means that each year this area has a chance of flooding of between 1% (1 in 100) and 3.3% (1 in 30).')
+          riskDetailPage.assert.containsText('@riverAndSeaPanel', 'Medium risk means that each year this area has a chance of flooding of between 1% and 3.3%.')
           break
         case RiskLevel.Low:
-          riskDetailPage.assert.containsText('@riverAndSeaPanel', 'Low risk means that each year this area has a chance of flooding of between 0.1% (1 in 1000) and 1% (1 in 100).')
+          riskDetailPage.assert.containsText('@riverAndSeaPanel', 'Low risk means that each year this area has a chance of flooding of between 0.1% and 1%.')
           break
         case RiskLevel.VeryLow:
-          riskDetailPage.assert.containsText('@riverAndSeaPanel', 'Very low risk means that each year this area has a chance of flooding of less than 0.1% (1 in 1000).')
+          riskDetailPage.assert.containsText('@riverAndSeaPanel', 'Very low risk means that each year this area has a chance of flooding of less than 0.1%.')
           break
         default:
           throw new Error('Invalid Risk Level', info.riverAndSeaRisk)
@@ -34,16 +34,16 @@ module.exports = {
       riskDetailPage.assert.containsText('@surfaceWaterPanel', 'The flood risk from surface water is ' + info.surfaceWaterRisk.toLowerCase())
       switch (info.surfaceWaterRisk) {
         case RiskLevel.High:
-          riskDetailPage.assert.containsText('@surfaceWaterPanel', 'High risk means that each year this area has a chance of flooding of greater than 3.3% (1 in 30).')
+          riskDetailPage.assert.containsText('@surfaceWaterPanel', 'High risk means that each year this area has a chance of flooding of greater than 3.3%.')
           break
         case RiskLevel.Medium:
-          riskDetailPage.assert.containsText('@surfaceWaterPanel', 'Medium risk means that each year this area has a chance of flooding of between 1% (1 in 100) and 3.3% (1 in 30).')
+          riskDetailPage.assert.containsText('@surfaceWaterPanel', 'Medium risk means that each year this area has a chance of flooding of between 1% and 3.3%.')
           break
         case RiskLevel.Low:
-          riskDetailPage.assert.containsText('@surfaceWaterPanel', 'Low risk means that each year this area has a chance of flooding of between 0.1% (1 in 1000) and 1% (1 in 100).')
+          riskDetailPage.assert.containsText('@surfaceWaterPanel', 'Low risk means that each year this area has a chance of flooding of between 0.1% and 1%.')
           break
         case RiskLevel.VeryLow:
-          riskDetailPage.assert.containsText('@surfaceWaterPanel', 'Very low risk means that each year this area has a chance of flooding of less than 0.1% (1 in 1000).')
+          riskDetailPage.assert.containsText('@surfaceWaterPanel', 'Very low risk means that each year this area has a chance of flooding of less than 0.1%.')
           break
         default:
           throw new Error('Invalid Risk Level', info.riverAndSeaRisk)
