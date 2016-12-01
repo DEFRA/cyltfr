ea39a0425e718dbdcf957a482cb8a90e88763fd5
Tedd <tjmason.ea@gmail.com>
2016-12-01 11:50:35 +0000

    Proxy for banner

--- node/risk-app/lib/services/flood.js
+++ node/risk-app/lib/services/flood.js
@@ -1,10 +1,24 @@
-var util = require('../util')
 var config = require('../../config')
 var floodWarningsUrl = config.floodWarningsUrl
+var agent = null
+if (process.env.http_proxy) {
+  var HTTPProxyAgent = require('http-proxy-agent')
+  var proxy = process.env.http_proxy
+  agent = new HTTPProxyAgent(proxy)
+}
+var wreck = require('wreck').defaults({
+  agent: agent,
+  timeout: config.httpTimeoutMs
+})
 
 function findWarnings (location, callback) {
   var url = floodWarningsUrl + '/api/warnings?location=' + location
-  util.getJson(url, callback)
+  wreck.get(url, { json: true }, function (err, response, payload) {
+    if (err || response.statusCode !== 200) {
+      return callback(err || payload || new Error('Unknown error'))
+    }
+    callback(null, payload)
+  })
 }
 
 module.exports = {
