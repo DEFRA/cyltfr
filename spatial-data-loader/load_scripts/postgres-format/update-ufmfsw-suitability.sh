717201a7eb991b6ac83f1310c5f1bc630081ebab
Paul Shaw <pmshaw.ea@gmail.com>
2017-05-22 14:21:01 +0100

    added load script for surface water in postgres format

---
+++ spatial-data-loader/load_scripts/postgres-format/update-ufmfsw-suitability.sh
@@ -0,0 +1,17 @@
+s3cmd get s3://ltfri/data-updates/ufmfsw_suitability_bv_bng.zip --force
+
+unzip -o ufmfsw_suitability_bv_bng.zip
+
+sudo -u postgres -g postgres psql -d ${LTFRI_DB_NAME} -c "truncate table u_ltfri.ufmfsw_suitability_bv_bng"
+
+sudo -u postgres -g postgres pg_restore -d ${LTFRI_DB_NAME} < ufmfsw_suitability_bv_bng.backup
+
+sudo -u postgres -g postgres psql -d ${LTFRI_DB_NAME} -c "SELECT setval('u_ltfri.ufmfsw_suitability_bv_bng_ogc_fid_seq', 8266, true)"
+
+sudo -u postgres -g postgres psql -d ${LTFRI_DB_NAME} -c "CLUSTER u_ltfri.ufmfsw_suitability_bv_bng USING ufmfsw_suitability_bv_bng_wkb_geometry_geom_idx"
+
+sudo -u postgres -g postgres psql -d ${LTFRI_DB_NAME} -c "VACUUM (ANALYZE) u_ltfri.ufmfsw_suitability_bv_bng"
+
+rm ufmfsw_suitability_bv_bng.zip
+
+rm ufmfsw_suitability_bv_bng.backup
