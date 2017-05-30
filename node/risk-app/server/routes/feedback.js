6930db29708210b115e9d6073c6d701cb01f44f3
Tedd Mason <tjmason.ea@gmail.com>
2017-05-30 13:51:17 +0100

    FLO-2148, user-agent for feedback

--- node/risk-app/server/routes/feedback.js
+++ node/risk-app/server/routes/feedback.js
@@ -4,13 +4,13 @@ module.exports = {
   config: {
     description: 'Get the feedback page',
     handler: function (request, reply) {
-      var ref = ''
-      if (request.info.referrer && request.info.referrer.indexOf('/feedback') === -1) {
-        ref = request.info.referrer
-      } else {
-        ref = request.server.info.protocol + '://' + request.info.host
-      }
-      reply.view('feedback', { ref: encodeURIComponent(ref), feedback: false })
+      var ref = (request.info.referrer && request.info.referrer.indexOf('/feedback') === -1) ? request.info.referrer : request.server.info.protocol + '://' + request.info.host
+      reply.view('feedback', {
+        ref: encodeURIComponent(ref),
+        feedback: false,
+        pageTitle: 'Provide feedback about this service - GOV.UK',
+        userAgent: encodeURIComponent(request.headers['user-agent'] ? request.headers['user-agent'] : '')
+      })
     }
   }
 }
