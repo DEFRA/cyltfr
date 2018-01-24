1e8ce1281c761276f420483bd05f388199751d83
EADEV <eadev@ip-10-65-68-238.eu-west-1.compute.internal>
2018-01-24 12:37:31 +0000

    Update versions

--- node/risk-service/index.js
+++ node/risk-service/index.js
@@ -1,40 +1,11 @@
-const composeServer = require('./server')
-const pkg = require('./package.json')
-const appName = pkg.name
-const appVersion = pkg.version
+const glupe = require('glupe')
+const { manifest, options } = require('./server')
 
-if (!module.parent) {
-  // There's no callee so we're running
-  // normally and will compose and start a server
-  composeServer()
-    .then(async server => {
-      /**
-       * Start the server
-       */
-      const details = {
-        name: appName,
-        version: appVersion,
-        uri: server.info.uri
-      }
-
-      try {
-        await server.start()
-
-        details.message = 'Started ' + details.name
-        server.log('info', details)
-        console.info(details.message)
-      } catch (err) {
-        details.error = err
-        details.message = 'Failed to start ' + details.name
-        server.log(['error', 'info'], details)
-        throw err
-      }
-    })
-    .catch(err => {
-      throw err
-    })
-} else {
-  // There's a callee so we're probably running a test.
-  // In which case just export the compose server function
-  module.exports = composeServer
-}
+;(async () => {
+  try {
+    await glupe(manifest, options)
+  } catch (err) {
+    process.exit(1)
+    console.error(err)
+  }
+})()
