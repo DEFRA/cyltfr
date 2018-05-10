14636cb98b348872c1b2996b5e7edbf91089000c
lcurryea <lcurry.ea@gmail.com>
2018-05-10 11:25:35 +0100

    omit map tests from build

--- node/risk-app-tests/config/nightwatch.conf.js
+++ node/risk-app-tests/config/nightwatch.conf.js
@@ -32,6 +32,7 @@ module.exports = {
   },
   test_settings: {
     'default': {
+      skiptags: 'map',
       // filter: 'tests/happy-path/index.js',
       launch_url: url,
       selenium_host: '127.0.0.1',
--- node/risk-app-tests/tests/map/index.js
+++ node/risk-app-tests/tests/map/index.js
@@ -1,6 +1,7 @@
 var mapTests = require('../../common/map')
 var data = require('./data.js')
 module.exports = {
+  '@tags': ['map'],
   'beforeEach': function (client) {
     // Force browser to pretendo-mobile
     // client.resizeWindow(300, 600)
