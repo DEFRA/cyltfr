cb94d4e4c4f8b5d44bead19ef2193f8cb29cb5e8
Tedd Mason <teddmason@gmail.com>
2018-11-16 15:28:51 +0000

    script updates for latest ROFRS work

--- spatial-data-loader/load_scripts/rofrs_england/load-data.sh
+++ spatial-data-loader/load_scripts/rofrs_england/load-data.sh
@@ -2,7 +2,7 @@
 
 echo Loading ROFRS England into database
 
-ogr2ogr -a_srs "EPSG:27700" -f "PostgreSQL" PG:"host=${LTFRI_DB_HOST} port=${LTFRI_DB_PORT} dbname=${LTFRI_DB_NAME} user=${LTFRI_DB_USER} password=${LTFRI_DB_PASSWORD}" ${LTFRI_GDB_ROOT}RoFRS_England${LTFRI_GDB_POSTFIX}.gdb -nln ${LTFRI_DB_SCHEMA}.rofrs_england_bv_bng -overwrite -progress -unsetFid --config PG_USE_COPY YES -lco SPATIAL_INDEX=${LTFRI_GENERATE_SPATIAL_INDEX} --config SPATIAL_INDEX NO RoFRS_England
+ogr2ogr -a_srs "EPSG:27700" -f "PostgreSQL" PG:"host=${LTFRI_DB_HOST} port=${LTFRI_DB_PORT} dbname=${LTFRI_DB_NAME} user=${LTFRI_DB_USER} password=${LTFRI_DB_PASSWORD}" ${LTFRI_GDB_ROOT}RoFRS_v201812.gdb -nln ${LTFRI_DB_SCHEMA}.rofrs_england_bv_bng -overwrite -progress -unsetFid --config PG_USE_COPY YES -lco SPATIAL_INDEX=${LTFRI_GENERATE_SPATIAL_INDEX} --config SPATIAL_INDEX NO RoFRS_v201812
 
 echo Finished loading ROFRS England
 
