294d8c929b11a2c36cfcdfded404498365d2a8a9
Chris Capper <chris.capperea@gmail.com>
2018-12-05 09:27:36 +0000

    Extra data added to happy path tests scenarios - ltrfi release 5th Dec 2018

--- node/risk-app-tests/tests/happy-path/data.js
+++ node/risk-app-tests/tests/happy-path/data.js
@@ -9,77 +9,91 @@ const data = [
 
   //  { postcode: 'POSTCODE', address: xx77079867, outcome: RiskStatus.AtRisk,
   //  riverAndSeaRisk: RiskLevel.High, surfaceWaterRisk: RiskLevel.High, reservoirRisk: true },
-  { premises: 2,
+  {
+    premises: 2,
     postcode: 'LE2 7QA',
     address: 2465055337,
     outcome: RiskStatus.AtRisk,
     riverAndSeaRisk: RiskLevel.High,
     surfaceWaterRisk: RiskLevel.High,
-    reservoirRisk: true },
+    reservoirRisk: true
+  },
   //  { postcode: 'POSTCODE', address: xx77079867, outcome: RiskStatus.AtRisk,
   //  riverAndSeaRisk: RiskLevel.High, surfaceWaterRisk: RiskLevel.Medium, reservoirRisk: true },
-  { premises: 101,
+  {
+    premises: 101,
     postcode: 'LE4 5QD',
     address: 2465065006,
     outcome: RiskStatus.AtRisk,
     riverAndSeaRisk: RiskLevel.High,
     surfaceWaterRisk: RiskLevel.Medium,
-    reservoirRisk: false },
+    reservoirRisk: false
+  },
   //  { postcode: 'POSTCODE', address: XX2465065006, outcome: RiskStatus.AtRisk,
   // riverAndSeaRisk: RiskLevel.High, surfaceWaterRisk: RiskLevel.Medium, reservoirRisk: true },
-  { premises: 65,
+  {
+    premises: 65,
     postcode: 'LE4 7SG',
     address: 2465089742,
     outcome: RiskStatus.AtRisk,
     riverAndSeaRisk: RiskLevel.High,
     surfaceWaterRisk: RiskLevel.Low,
-    reservoirRisk: false },
+    reservoirRisk: false
+  },
   //  { postcode: 'POSTCODE', address: XX, outcome: RiskStatus.AtRisk,
   //  riverAndSeaRisk: RiskLevel.High, surfaceWaterRisk: RiskLevel.VeryLow, reservoirRisk: true },
   //  { postcode: 'POSTCODE', address: xx77079867, outcome: RiskStatus.AtRisk,
   //  riverAndSeaRisk: RiskLevel.High, surfaceWaterRisk: RiskLevel.VeryLow, reservoirRisk: false },
 
   // Medium R&S Risk with variant SW Risk//
-  { premises: 11,
+  {
+    premises: 11,
     postcode: 'CV37 6YZ',
     address: 100070216073,
     outcome: RiskStatus.AtRisk,
     riverAndSeaRisk: RiskLevel.Medium,
     surfaceWaterRisk: RiskLevel.High,
-    reservoirRisk: true },
+    reservoirRisk: true
+  },
   //  { postcode: 'POSTCODE', address: 77079867, outcome: RiskStatus.AtRisk,
   //  riverAndSeaRisk: RiskLevel.Medium, surfaceWaterRisk: RiskLevel.High, reservoirRisk: false },
-  { premises: '20',
+  {
+    premises: '20',
     postcode: 'LE3 5FD',
     address: 2465041934,
     outcome: RiskStatus.AtRisk,
     riverAndSeaRisk: RiskLevel.Medium,
     surfaceWaterRisk: RiskLevel.High,
-    reservoirRisk: true },
+    reservoirRisk: true
+  },
   //  { postcode: 'POSTCODE', address: xx77079867, outcome: RiskStatus.AtRisk,
   //  riverAndSeaRisk: RiskLevel.Medium, surfaceWaterRisk: RiskLevel.Medium, reservoirRisk: false },
   //  { postcode: 'POSTCODE', address: xx77079867, outcome: RiskStatus.AtRisk,
   //  riverAndSeaRisk: RiskLevel.Medium, surfaceWaterRisk: RiskLevel.Low, reservoirRisk: true },
-  { premises: 'THE STABLES',
+  {
+    premises: 'THE STABLES',
     postcode: 'NN6 8PH',
     address: 28025584,
     outcome: RiskStatus.AtRisk,
     riverAndSeaRisk: RiskLevel.Medium,
     surfaceWaterRisk: RiskLevel.Low,
-    reservoirRisk: false },
+    reservoirRisk: false
+  },
   //  { postcode: 'POSTCODE', address: xx77079867, outcome: RiskStatus.AtRisk,
   //  riverAndSeaRisk: RiskLevel.Medium, surfaceWaterRisk: RiskLevel.VeryLow, reservoirRisk: true },
   //  { postcode: 'POSTCODE', address: xx77079867, outcome: RiskStatus.AtRisk,
   //  riverAndSeaRisk: RiskLevel.Medium, surfaceWaterRisk: RiskLevel.VeryLow, reservoirRisk: false },
 
   // Low R&S Risk with variant SW Risk//
-  { premises: 7,
+  {
+    premises: 7,
     postcode: 'SW18 1TJ',
     address: 121008673,
     outcome: RiskStatus.AtRisk,
     riverAndSeaRisk: RiskLevel.Low,
     surfaceWaterRisk: RiskLevel.High,
-    reservoirRisk: false },
+    reservoirRisk: false
+  },
   // { premises: 70,
   //   postcode: 'W6 0XL',
   //   address: 34002373,
@@ -89,27 +103,33 @@ const data = [
   //   reservoirRisk: false },
   //  { postcode: 'POSTCODE', address: XX77079867, outcome: RiskStatus.AtRisk,
   //  riverAndSeaRisk: RiskLevel.Low, surfaceWaterRisk: RiskLevel.Medium, reservoirRisk: true },
-  { premises: 21,
+  {
+    premises: 21,
     postcode: 'LE3 5JR',
     address: 2465038757,
     outcome: RiskStatus.AtRisk,
     riverAndSeaRisk: RiskLevel.Low,
     surfaceWaterRisk: RiskLevel.Medium,
-    reservoirRisk: false },
-  { premises: 2,
+    reservoirRisk: false
+  },
+  {
+    premises: 2,
     postcode: 'NN6 6JP',
     address: 28030078,
     outcome: RiskStatus.AtRisk,
     riverAndSeaRisk: RiskLevel.Low,
     surfaceWaterRisk: RiskLevel.Low,
-    reservoirRisk: true },
-  { premises: 21,
+    reservoirRisk: true
+  },
+  {
+    premises: 21,
     postcode: 'SW13 0DS',
     address: 100022295693,
     outcome: RiskStatus.AtRisk,
     riverAndSeaRisk: RiskLevel.Low,
     surfaceWaterRisk: RiskLevel.Medium,
-    reservoirRisk: true },
+    reservoirRisk: true
+  },
   // { premises: 28,
   //   postcode: 'TW9 3BH',
   //   address: 100022308486,
@@ -126,13 +146,15 @@ const data = [
   //   reservoirRisk: false },
 
   // Very Low R&S Risk with variant SW Risk//
-  { premises: '27A',
+  {
+    premises: '27A',
     postcode: 'S6 4JP',
     address: 100052100616,
     outcome: RiskStatus.AtRisk,
     riverAndSeaRisk: RiskLevel.Medium,
     surfaceWaterRisk: RiskLevel.High,
-    reservoirRisk: true },
+    reservoirRisk: true
+  },
   // { postcode: 'SS8 9RH', address: 100090372402, outcome: RiskStatus.AtRisk,
   // riverAndSeaRisk: RiskLevel.VeryLow, surfaceWaterRisk: RiskLevel.High, reservoirRisk: false },
   //  { postcode: 'POSTCODE', address: xx77079867, outcome: RiskStatus.AtRisk,
@@ -141,13 +163,15 @@ const data = [
   //  riverAndSeaRisk: RiskLevel.VeryLow, surfaceWaterRisk: RiskLevel.Medium, reservoirRisk: false },
   // { postcode: 'POSTCODE', address: 100090378533, outcome: RiskStatus.AtRisk,
   // riverAndSeaRisk: RiskLevel.VeryLow, surfaceWaterRisk: RiskLevel.Low, reservoirRisk: true },
-  { premises: 42,
+  {
+    premises: 42,
     postcode: 'SS8 7EH',
     address: 100090373928,
     outcome: RiskStatus.AtRisk,
     riverAndSeaRisk: RiskLevel.VeryLow,
     surfaceWaterRisk: RiskLevel.Low,
-    reservoirRisk: false },
+    reservoirRisk: false
+  },
   //  { postcode: 'POSTCODE', address: xx77079867, outcome: RiskStatus.AtRisk,
   //  riverAndSeaRisk: RiskLevel.VeryLow, surfaceWaterRisk: RiskLevel.VeryLow, reservoirRisk: true },
   //  { postcode: 'POSTCODE', address: XX, outcome: RiskStatus.AtRisk,
@@ -173,13 +197,15 @@ const data = [
   //  riverAndSeaRisk: RiskLevel.High, surfaceWaterRisk: RiskLevel.VeryLow, reservoirRisk: false },
 
   // Medium R&S Risk with variant SW Risk//
-  { premises: 83,
+  {
+    premises: 83,
     postcode: 'LE3 9RD',
     address: 2465041144,
     outcome: RiskStatus.AtRiskMonitor, // Target area https://flood-warning-information.service.gov.uk/target-area/034WAF402 expanded February 2018
     riverAndSeaRisk: RiskLevel.Low,
     surfaceWaterRisk: RiskLevel.High,
-    reservoirRisk: true },
+    reservoirRisk: true
+  },
   //  { postcode: 'POSTCODE', address: 77079867, outcome: RiskStatus.AtRiskMonitor,
   //  riverAndSeaRisk: RiskLevel.Medium, surfaceWaterRisk: RiskLevel.High, reservoirRisk: false },
   //  { postcode: 'POSTCODE', address: xx77079867, outcome: RiskStatus.AtRiskMonitor,
@@ -188,37 +214,43 @@ const data = [
   // riverAndSeaRisk: RiskLevel.Medium, surfaceWaterRisk: RiskLevel.Medium, reservoirRisk: false },
   // { postcode: 'POSTCODE', address: xx77079867, outcome: RiskStatus.AtRiskMonitor,
   // riverAndSeaRisk: RiskLevel.Medium, surfaceWaterRisk: RiskLevel.Low, reservoirRisk: true },
-  { premises: 43,
+  {
+    premises: 43,
     postcode: 'LE3 6AH',
     address: 2465040718,
     outcome: RiskStatus.AtRiskMonitor,
     riverAndSeaRisk: RiskLevel.High,
     surfaceWaterRisk: RiskLevel.High,
-    reservoirRisk: false },
+    reservoirRisk: false
+  },
   //  { postcode: 'POSTCODE', address: xx77079867, outcome: RiskStatus.AtRiskMonitor,
   //  riverAndSeaRisk: RiskLevel.Medium, surfaceWaterRisk: RiskLevel.VeryLow, reservoirRisk: true },
   // { postcode: 'POSTCODE', address: xx77079867, outcome: RiskStatus.AtRiskMonitor,
   // riverAndSeaRisk: RiskLevel.Medium, surfaceWaterRisk: RiskLevel.VeryLow, reservoirRisk: false },
 
   // Low R&S Risk with variant SW Risk//
-  { premises: 73,
+  {
+    premises: 73,
     postcode: 'LE3 9RD',
     address: 2465041139,
     outcome: RiskStatus.AtRiskMonitor,
     riverAndSeaRisk: RiskLevel.Medium,
     surfaceWaterRisk: RiskLevel.High,
-    reservoirRisk: true },
+    reservoirRisk: true
+  },
   //  { postcode: 'POSTCODE', address: xxx, outcome: RiskStatus.AtRiskMonitor,
   //  riverAndSeaRisk: RiskLevel.Low, surfaceWaterRisk: RiskLevel.High, reservoirRisk: false },
   //  { postcode: 'POSTCODE', address: XX77079867, outcome: RiskStatus.AtRiskMonitor,
   //  riverAndSeaRisk: RiskLevel.Low, surfaceWaterRisk: RiskLevel.Medium, reservoirRisk: true },
-  { premises: 3,
+  {
+    premises: 3,
     postcode: 'NN6 6LE',
     address: 28015435,
     outcome: RiskStatus.AtRiskMonitor,
     riverAndSeaRisk: RiskLevel.VeryLow,
     surfaceWaterRisk: RiskLevel.Medium,
-    reservoirRisk: false },
+    reservoirRisk: false
+  },
 
   // Very Low R&S Risk with variant SW Risk//
   //  { postcode: 'POSTCODE', address: XX879331, outcome: RiskStatus.AtRiskMonitor,
@@ -227,8 +259,15 @@ const data = [
   //  riverAndSeaRisk: RiskLevel.VeryLow, surfaceWaterRisk: RiskLevel.High, reservoirRisk: false },
   //  { postcode: 'POSTCODE', address: xx77079867, outcome: RiskStatus.AtRiskMonitor,
   //  riverAndSeaRisk: RiskLevel.VeryLow, surfaceWaterRisk: RiskLevel.Medium, reservoirRisk: true },
-  //  { postcode: 'POSTCODE', address: xx77079867, outcome: RiskStatus.AtRiskMonitor,
-  //  riverAndSeaRisk: RiskLevel.VeryLow, surfaceWaterRisk: RiskLevel.Medium, reservoirRisk: false },
+  {
+    premises: 2,
+    postcode: 'ME10 2PS',
+    address: 100061090397,
+    outcome: RiskStatus.AtRiskMonitor,
+    riverAndSeaRisk: RiskLevel.VeryLow,
+    surfaceWaterRisk: RiskLevel.Medium,
+    reservoirRisk: false
+  },
 
   // Low Risk permuatations //
   //  { postcode: 'POSTCODE', address: XX33001217, outcome: RiskStatus.LowRisk,
@@ -239,31 +278,37 @@ const data = [
   //  riverAndSeaRisk: RiskLevel.Low, surfaceWaterRisk: RiskLevel.VeryLow, reservoirRisk: false },
   //  { postcode: 'POSTCODE', address: XX200000879311, outcome: RiskStatus.LowRisk,
   //  riverAndSeaRisk: RiskLevel.VeryLow, surfaceWaterRisk: RiskLevel.Low, reservoirRisk: true },
-  { premises: 6,
+  {
+    premises: 6,
     postcode: 'BS20 6BQ',
     address: 24063776,
     outcome: RiskStatus.LowRisk,
     riverAndSeaRisk: RiskLevel.VeryLow,
     surfaceWaterRisk: RiskLevel.Low,
-    reservoirRisk: false },
-  { premises: 12,
+    reservoirRisk: false
+  },
+  {
+    premises: 12,
     postcode: 'LE4 9BH',
     address: 2465119536,
     outcome: RiskStatus.LowRisk,
     riverAndSeaRisk: RiskLevel.Low,
     surfaceWaterRisk: RiskLevel.Low,
-    reservoirRisk: true },
+    reservoirRisk: true
+  },
   //  { postcode: 'POSTCODE', address: xx77079867, outcome: RiskStatus.LowRisk,
   //  riverAndSeaRisk: RiskLevel.Low, surfaceWaterRisk: RiskLevel.Low, reservoirRisk: false },
 
   // Very low risk //
-  { premises: 'Kenyon',
+  {
+    premises: 'Kenyon',
     postcode: 'WA3 7ED',
     address: 200000977803,
     outcome: RiskStatus.VeryLowRisk,
     riverAndSeaRisk: RiskLevel.VeryLow,
     surfaceWaterRisk: RiskLevel.VeryLow,
-    reservoirRisk: false }
+    reservoirRisk: false
+  }
 
 ]
 
