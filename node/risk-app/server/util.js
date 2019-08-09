ecb4a579cda8b9a97f7fc00053075b325ac3fe58
David Stone <davidjamesstone@gmail.com>
2019-08-09 16:39:45 +0100

    Test

--- node/risk-app/server/util.js
+++ node/risk-app/server/util.js
@@ -6,6 +6,8 @@ const wreck = require('@hapi/wreck').defaults({
 })
 let wreckExt
 
+const http = require('../test/ralph')
+
 if (config.http_proxy) {
   wreckExt = require('@hapi/wreck').defaults({
     timeout: config.httpTimeoutMs,
