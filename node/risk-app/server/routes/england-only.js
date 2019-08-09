e6aea6f2eb9250e4f0cb0223297ee0e98d5e565c
David Stone <davidjamesstone@gmail.com>
2019-08-09 18:22:06 +0100

    Test

--- node/risk-app/server/routes/england-only.js
+++ node/risk-app/server/routes/england-only.js
@@ -1,20 +1,16 @@
 const joi = require('@hapi/joi')
-const wreck = require('@hapi/wreck')
+
 module.exports = {
   method: 'GET',
   path: '/england-only',
   handler: async (request, h) => {
+    const model = {
+      isWales: request.query.region === 'wales',
+      isScotland: request.query.region === 'scotland',
+      isNorthernIreland: request.query.region === 'northern-ireland'
+    }
 
-    const url = 'http://internal-DEVLTFBESELB001-138284812.eu-west-1.elb.amazonaws.com:8050/floodrisk/364932/373262/20'
-    const { res, payload } = await wreck.get(url)
-    return payload
-    // const model = {
-    //   isWales: request.query.region === 'wales',
-    //   isScotland: request.query.region === 'scotland',
-    //   isNorthernIreland: request.query.region === 'northern-ireland'
-    // }
-
-    // return h.view('england-only', model)
+    return h.view('england-only', model)
   },
   options: {
     description: 'Get the england only page',
