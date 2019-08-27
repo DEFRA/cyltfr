6635eaf9482a2e7ec43d1fc82437b098e5755f70
David Stone <davidjamesstone@gmail.com>
2019-08-27 12:32:54 +0100

    Proxy test

--- node/risk-app/server/routes/test.js
+++ node/risk-app/server/routes/test.js
@@ -14,39 +14,40 @@ module.exports = [{
     const proxyUrl2 = getProxyForUrl(findByIdUri)
     const proxyUrl1 = getProxyForUrl(findByPostcodeUri)
 
-    // return {
-    //   proxyUrl1,
-    //   proxyUrl2,
-    //   http_proxy: process.env.http_proxy,
-    //   https_proxy: process.env.https_proxy
-    // }
-
-    const o = { proxyUrl1, proxyUrl2, config }
-
-    try {
-      const wreck = config.http_proxy ? require('@hapi/wreck').defaults({
-        timeout: config.httpTimeoutMs,
-        agent: new HttpsProxyAgent(config.http_proxy)
-      }) : require('@hapi/wreck')
-
-      const data = await wreck.get(findByPostcodeUri, { json: true })
-
-      return {
-        o,
-        data: data.payload,
-        proxyUrl1,
-        proxyUrl2,
-        http_proxy: process.env.HTTP_PROXY,
-        https_proxy: process.env.HTTPS_PROXY
-      }
-    } catch (err) {
-      return {
-        o,
-        proxyUrl1,
-        proxyUrl2,
-        http_proxy: process.env.http_proxy,
-        https_proxy: process.env.https_proxy
-      }
+    return {
+      proxyUrl1,
+      proxyUrl2,
+      http_proxy: process.env.http_proxy,
+      https_proxy: process.env.https_proxy
     }
+
+    // const o = { proxyUrl1, proxyUrl2, config }
+
+    // try {
+    //   const wreck = config.http_proxy ? require('@hapi/wreck').defaults({
+    //     timeout: config.httpTimeoutMs,
+    //     agent: new HttpsProxyAgent(config.http_proxy)
+    //   }) : require('@hapi/wreck')
+
+    //   const data = await wreck.get(findByPostcodeUri, { json: true })
+
+    //   return {
+    //     o,
+    //     data: data.payload,
+    //     proxyUrl1,
+    //     proxyUrl2,
+    //     http_proxy: process.env.HTTP_PROXY,
+    //     https_proxy: process.env.HTTPS_PROXY
+    //   }
+    // } catch (err) {
+    //   return {
+    //     o,
+    //     err,
+    //     proxyUrl1,
+    //     proxyUrl2,
+    //     http_proxy: process.env.http_proxy,
+    //     https_proxy: process.env.https_proxy
+    //   }
+    // }
   }
 }]
