381d929d799e29fa421eba257c8f92e6f218461d
David <dstone1.ea@gmail.com>
2020-03-09 12:59:20 +0000

    Feature/ltfri 153 os base maps

--- node/risk-admin/server/models/comment-edit.js
+++ node/risk-admin/server/models/comment-edit.js
@@ -1,7 +1,8 @@
 class CommentEdit {
-  constructor (comment, geometry, auth) {
+  constructor (comment, geometry, auth, capabilities) {
     this.comment = comment
     this.geometry = geometry
+    this.capabilities = capabilities
     this.isApprover = auth.credentials.isApprover
     this.allowDelete = auth.credentials.isApprover ||
       comment.createdBy === auth.credentials.profile.email
--- node/risk-admin/server/public/static/js/comment-map.js
+++ node/risk-admin/server/public/static/js/comment-map.js
@@ -1,5 +1,5 @@
 ;(function () {
-  function commentMap (geojson, target, title) {
+  function commentMap (geojson, target, capabilities, title) {
     var ol = window.ol
     var proj4 = window.proj4
 
@@ -33,17 +33,39 @@
     var proj27700 = ol.proj.get('EPSG:27700')
     proj27700.setExtent([0, 0, 700000, 1300000])
 
+    var parser = new ol.format.WMTSCapabilities()
+    var result = parser.read(capabilities)
+
+    result.OperationsMetadata.GetTile.DCP.HTTP.Get[0].href = result.OperationsMetadata.GetTile.DCP.HTTP.Get[0].href.replace('http://', 'https://')
+
+    var options = ol.source.WMTS.optionsFromCapabilities(result, {
+      layer: 'osgb',
+      matrixSet: 'ZoomMap',
+      crossOrigin: 'anonymous'
+    })
+
+    var source = new ol.source.WMTS(options)
+
+    // array of ol.tileRange can't find any reference to this object in ol3 documentation, but is set to NaN and stops the map from functioning
+    // openlayers doesn't expose fulltileranges as a property, so when using minified ol have to set tilegrid.a to null, which is what fulltileranges
+    // is mapped as, hopefully OS will fix their service, otherwise something more robust needs sorting out
+    source.tileGrid.fullTileRanges_ = null
+    source.tileGrid.a = null
+
+    var layer = new ol.layer.Tile({
+      ref: 'osgb',
+      source: source
+    })
+
     var map = new ol.Map({
       target: target,
       view: new ol.View({
-        projection: 'EPSG:3857',
+        projection: proj27700,
         center: [0, 0],
         zoom: 2
       }),
       layers: [
-        new ol.layer.Tile({
-          source: new ol.source.OSM()
-        }),
+        layer,
         vectorLayer
       ],
       controls: ol.control.defaults({ attribution: false })
