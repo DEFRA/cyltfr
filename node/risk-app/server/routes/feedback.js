2493cdaad6b0dfaee979a85d711120e63133647e
David <dstone1.ea@gmail.com>
2020-03-16 14:58:55 +0000

    Reinstate smart survey

--- node/risk-app/server/routes/feedback.js
+++ node/risk-app/server/routes/feedback.js
@@ -4,21 +4,20 @@ module.exports = {
   options: {
     description: 'Get the feedback page',
     handler: (request, h) => {
-      return h.redirect('https://defragroup.eu.qualtrics.com/jfe/form/SV_eVcuXnaMi8wiVkF')
-      // const ref = (request.info.referrer && request.info.referrer.indexOf('/feedback') === -1)
-      //   ? request.info.referrer
-      //   : request.server.info.protocol + '://' + request.info.host
+      const ref = (request.info.referrer && request.info.referrer.indexOf('/feedback') === -1)
+        ? request.info.referrer
+        : request.server.info.protocol + '://' + request.info.host
 
-      // const agent = request.headers['user-agent']
-      //   ? request.headers['user-agent']
-      //   : ''
+      const agent = request.headers['user-agent']
+        ? request.headers['user-agent']
+        : ''
 
-      // return h.view('feedback', {
-      //   ref: encodeURIComponent(ref),
-      //   feedback: false,
-      //   pageTitle: 'Provide feedback about this service - GOV.UK',
-      //   userAgent: encodeURIComponent(agent)
-      // })
+      return h.view('feedback', {
+        ref: encodeURIComponent(ref),
+        feedback: false,
+        pageTitle: 'Provide feedback about this service',
+        userAgent: encodeURIComponent(agent)
+      })
     }
   }
 }
