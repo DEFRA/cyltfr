18fb8ae7345806074ff8bac0d19bf2a9a5a296ba
David Stone <davidjamesstone@gmail.com>
2022-03-17 13:31:16 +0000

    Update dependencies

--- node/risk-admin/server/plugins/logging.js
+++ node/risk-admin/server/plugins/logging.js
@@ -3,8 +3,6 @@ const config = require('../config')
 module.exports = {
   plugin: require('hapi-pino'),
   options: {
-    logPayload: true,
-    prettyPrint: config.isDev,
     level: config.isDev ? 'debug' : 'warn'
   }
 }
