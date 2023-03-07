cc11c686127bc30adb6ffd6cc1eabab02769b892
olummide dele-johnson <olummide.dele-johnson@capgemini.com>
2023-03-07 13:51:27 +0000

    LTFRI-587 remove console.log in index page

--- node/risk-app/client/js/captcha/index.js
+++ node/risk-app/client/js/captcha/index.js
@@ -1,5 +1,3 @@
-
-console.log("Loading...")
 async function myCallback (solution, captchaUrl, captchaSiteKey) {
   console.log('Captcha finished with solution ' + solution)
   const url = `${captchaUrl}`
