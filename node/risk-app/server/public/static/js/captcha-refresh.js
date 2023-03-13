83fb9d65570fbdcd0024c53fb7b1c02172f66a71
olummide dele-johnson <olummide.dele-johnson@capgemini.com>
2023-03-13 16:34:25 +0000

    hotfix-friendly-captcha moce ie-text to postcode view page

--- node/risk-app/server/public/static/js/captcha-refresh.js
+++ node/risk-app/server/public/static/js/captcha-refresh.js
@@ -10,10 +10,13 @@ window.addEventListener( "pageshow", function ( event ) {
 });
 if (!!document.documentMode) {
   // Only true in Internet Explorer
-  Array.prototype.slice.call(document.querySelectorAll(".frc-captcha")).forEach(function (element) {
-    var messageElement = document.createElement("p");
-    messageElement.innerHTML =
-      "The anti-robot check works better and faster in modern browsers such as Edge, Firefox, or Chrome. Please consider updating your browser";
-    element.parentNode.insertBefore(messageElement, element.nextSibling);
-  });
+  // Array.prototype.slice.call(document.querySelectorAll(".frc-captcha")).forEach(function (element) {
+  //   var messageElement = document.createElement("p");
+  //   messageElement.innerHTML =
+  //     "The anti-robot check works better and faster in modern browsers such as Edge, Firefox, or Chrome. Please consider updating your browser";
+  //   element.parentNode.insertBefore(messageElement, element.nextSibling);
+  // });
+  const ieBrowser = document.getElementById('nc-browser')
+  ieBrowser.style.display = 'block'
+
 }
