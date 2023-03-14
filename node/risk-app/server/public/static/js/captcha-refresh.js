d85a2b88eab19c7e4dd29bbe1b1779f48468ac41
Olumide Dele-Johnson <olumide.dele-johnson@capgemini.com>
2023-03-14 09:46:58 +0000

    Bug fix to friendly captcha feature

---
+++ node/risk-app/server/public/static/js/captcha-refresh.js
@@ -0,0 +1,22 @@
+
+window.addEventListener( "pageshow", function ( event ) {
+  var historyTraversal = event.persisted || 
+                         ( typeof window.performance != "undefined" && 
+                              window.performance.navigation.type === 2 );
+  if ( historyTraversal ) {
+    // Handle page restore.
+    window.location.reload();
+  }
+});
+if (!!document.documentMode) {
+  // Only true in Internet Explorer
+  // Array.prototype.slice.call(document.querySelectorAll(".frc-captcha")).forEach(function (element) {
+  //   var messageElement = document.createElement("p");
+  //   messageElement.innerHTML =
+  //     "The anti-robot check works better and faster in modern browsers such as Edge, Firefox, or Chrome. Please consider updating your browser";
+  //   element.parentNode.insertBefore(messageElement, element.nextSibling);
+  // });
+  const ieBrowser = document.getElementById('nc-browser')
+  ieBrowser.style.display = 'block'
+
+}
