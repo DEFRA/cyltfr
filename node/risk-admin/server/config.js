7d529253b817720aeed135955f95822198333c98
Matthew Collins <matthew.collins@defra.gov.uk>
2023-06-08 10:47:43 +0100

    Remove debug code

--- node/risk-admin/server/config.js
+++ node/risk-admin/server/config.js
@@ -38,7 +38,6 @@ const config = {
 // Validate config
 const { error, value } = schema.validate(config)
 
-console.log (process.env)
 // Throw if config is invalid
 if (error) {
   throw new Error(`The server config is invalid. ${error.message}`)
