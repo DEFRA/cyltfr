ae4ea6a9cc6a1c0cf54e8b49d301f958351e75bb
Matthew-Collins-Defra <48916014+Matthew-Collins-Defra@users.noreply.github.com>
2023-07-04 07:12:15 +0100

    Release 3.4.0
    * Fixes required for automated testing with Captcha bypass
    
    * LTFRI-526 Investigate using Erbit - Improve error logging across the service
    
    * Update package versions
    
    * Update app version
    
    * Fix proxy error (#7)
    
    ---------
    
    Co-authored-by: olummide dele-johnson <olummide.dele-johnson@capgemini.com>
    Co-authored-by: olumide <ogundeleolumuyiwaolumide@gmail.com>

--- node/risk-app/server/index.js
+++ node/risk-app/server/index.js
@@ -40,7 +40,12 @@ async function createServer () {
     require('../mock/address')
     server.log('info', 'Address server is being mocked')
   }
-
+  if (config.errbit.postErrors) {
+    await server.register({
+      plugin: require('./plugins/airbrake'),
+      options: config.errbit.options
+    })
+  }
   return server
 }
 
