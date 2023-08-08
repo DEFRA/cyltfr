acef4a40996a1c81bef093887dd0f8fa1b89536a
michael steinacher <steinacherm@hotmail.co.uk>
2023-08-08 10:47:44 +0100

    Reverted backlink and made it so if at map page it will go back to risk page. Risk page will redirect to postcode page if no address found.

--- node/risk-app/server/services/defineBackLink.js
+++ node/risk-app/server/services/defineBackLink.js
@@ -1,6 +1,10 @@
 function defineBackLink (currentPage, postcode) {
   if (currentPage === '/risk' || currentPage === '/england-only') {
+    console.log('currentPageInside', currentPage)
     return '/search?postcode=' + postcode
+  }
+  if (currentPage === '/map') {
+    return '/risk'
   }
   return '/postcode'
 }
