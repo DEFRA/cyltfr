131df858cd9a53b6d10d525df4ce44b5d8df38d4
michael steinacher <steinacherm@hotmail.co.uk>
2023-10-04 16:18:26 +0100

    Missed end of expect on test.

--- node/risk-app/test/unit/address.js
+++ node/risk-app/test/unit/address.js
@@ -6,6 +6,6 @@ const addressService = require('../../server/services/address')
 lab.experiment('Address service', () => {
   lab.test('capitaliseAddress function capitalises the first of each letter other than postcode which remains in capitals', async () => {
     const address = '28, NORTHFIELD CLOSE, NEWPORT, NP18 3EZ'
-    Code.expect(addressService.capitaliseAddress(address)).to.equal('28, Northfield, Newport, NP18 3EZ')
+    Code.expect(addressService.capitaliseAddress(address)).to.equal('28, Northfield Close, Newport, NP18 3EZ')
   })
 })
