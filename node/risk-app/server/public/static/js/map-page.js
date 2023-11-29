d1bd8f822f0d495b0927cb11655b2fcbc854b130
Michael Steinacher <49474728+MickStein@users.noreply.github.com>
2023-11-29 15:02:36 +0000

    Implement reservoirs extent button (#133)
    * Implement reservoirs extent button
    
    https://eaflood.atlassian.net/browse/LTFRI-860
    
    Users visiting the map and selecting the advanced options link they should see the reservoirs extent results on the map.

--- node/risk-app/server/public/static/js/map-page.js
+++ node/risk-app/server/public/static/js/map-page.js
@@ -49,7 +49,6 @@ function mapPage () {
   }
   const mapController = new MapController(mapCategories.categories)
   const $header = $('.govuk-radios')
-  // const $selector = $('input[name=measurements]')
   const $map = $('#map')
   const $body = $(document.body)
 
@@ -58,19 +57,24 @@ function mapPage () {
   const hasLocation = !!easting
   const maps = window.maps
 
-  maps.loadMap(hasLocation && [easting, northing])
+  maps.loadMap((hasLocation && [easting, northing]))
 
   // This function updates the map to the radio button you select (extent, depth, velocity)
   function setCurrent (ref) {
     mapController.setCurrent(ref)
-
+    const selectedAddressCheckbox = document.getElementById('selected-address-checkbox')
+    const showFloodingCheckbox = document.getElementById('display-layers-checkbox')
     const mapReferenceValue = selectedOption()
-    maps.showMap('risk:' + mapReferenceValue.substring(mapReferenceValue.indexOf('_') + 1))
+
+    if (showFloodingCheckbox.checked) {
+      maps.showMap('risk:' + mapReferenceValue.substring(mapReferenceValue.indexOf('_') + 1), selectedAddressCheckbox.checked)
+    } else {
+      maps.showMap(undefined, selectedAddressCheckbox.checked)
+    }
   }
 
   // Default to the first category/map
   maps.onReady(function () {
-    // Handle the mobile map selector change
     $header.on('change', 'input[name="measurements"]', function (e) {
       e.preventDefault()
       setCurrent($(this).val())
@@ -80,6 +84,10 @@ function mapPage () {
       setCurrent($(this).val())
     })
     $header.on('change', 'input[name="scenarios-velocity"]', function (e) {
+      e.preventDefault()
+      setCurrent($(this).val())
+    })
+    $header.on('change', 'input[name="map-toggle"]', function (e) {
       e.preventDefault()
       setCurrent($(this).val())
     })
@@ -95,87 +103,109 @@ function mapPage () {
 
 /* eslint-disable no-unused-vars */
 // This function adjusts the descriptions that appear/disappear depending on selected radio button
-function handleRadioChange (selected) {
+function handleRadioChange (selected, type) {
   const scenarioBarDepth = document.getElementById('scenario-container-depth')
   const scenarioBarVelocity = document.getElementById('scenario-container-velocity')
-  const extentInfo = document.getElementById('sw-extent-desc-container')
+  const extentInfoRs = document.getElementById('rs-extent-desc-container')
+  const extentInfoReservoirs = document.getElementById('reservoirs-extent-desc-container')
+  const extentInfoSw = document.getElementById('sw-extent-desc-container')
   const depthInfo = document.getElementById('sw-depth-desc-container')
   const velocityInfo = document.getElementById('sw-velocity-desc-container')
   const copyrightBtn = document.getElementById('att-key-copyright-btn')
   const copyrightInfo = document.getElementById('copyright-info-container')
   const olZoom = document.getElementsByClassName('ol-zoom')
 
-  if (selected === 'extent') {
-    extentInfo.style.display = 'block'
-    depthInfo.style.display = 'none'
-    velocityInfo.style.display = 'none'
-    scenarioBarDepth.style.display = 'none'
-    scenarioBarVelocity.style.display = 'none'
-    copyrightBtn.style.top = 'calc(100vh - 115px)'
-    copyrightInfo.style.right = '360px'
-    olZoom[0].style.top = 'calc(100% - 145px)'
-  }
   if (selected === 'depth') {
-    extentInfo.style.display = 'none'
+    extentInfoRs.style.display = 'none'
+    extentInfoReservoirs.style.display = 'none'
+    extentInfoSw.style.display = 'none'
     depthInfo.style.display = 'block'
     velocityInfo.style.display = 'none'
     scenarioBarDepth.style.display = 'block'
     scenarioBarVelocity.style.display = 'none'
     copyrightBtn.style.top = 'calc(100vh - 205px)'
     copyrightInfo.style.right = '310px'
+    copyrightInfo.style.display = 'none'
     olZoom[0].style.top = 'calc(100% - 235px)'
   }
   if (selected === 'velocity') {
-    extentInfo.style.display = 'none'
+    extentInfoRs.style.display = 'none'
+    extentInfoReservoirs.style.display = 'none'
+    extentInfoSw.style.display = 'none'
     depthInfo.style.display = 'none'
     velocityInfo.style.display = 'block'
     scenarioBarDepth.style.display = 'none'
     scenarioBarVelocity.style.display = 'block'
-    copyrightBtn.style.top = 'calc(100vh - 205px);'
+    copyrightBtn.style.top = 'calc(100vh - 205px)'
     copyrightInfo.style.right = '310px'
+    copyrightInfo.style.display = 'none'
     olZoom[0].style.top = 'calc(100% - 235px)'
+  }
+
+  if (selected === 'extent') {
+    if (type === 'reservoirs') {
+      extentInfoRs.style.display = 'none'
+      extentInfoReservoirs.style.display = 'block'
+      extentInfoSw.style.display = 'none'
+      depthInfo.style.display = 'none'
+      velocityInfo.style.display = 'none'
+      scenarioBarDepth.style.display = 'none'
+      scenarioBarVelocity.style.display = 'none'
+      copyrightBtn.style.top = 'calc(100vh - 115px)'
+      copyrightInfo.style.right = '360px'
+      olZoom[0].style.top = 'calc(100% - 145px)'
+    }
+    if (type === 'rivers and the sea') {
+      extentInfoRs.style.display = 'block'
+      extentInfoReservoirs.style.display = 'none'
+      extentInfoSw.style.display = 'none'
+      depthInfo.style.display = 'none'
+      velocityInfo.style.display = 'none'
+      scenarioBarDepth.style.display = 'none'
+      scenarioBarVelocity.style.display = 'none'
+      copyrightBtn.style.top = 'calc(100vh - 115px)'
+      copyrightInfo.style.right = '360px'
+      olZoom[0].style.top = 'calc(100% - 145px)'
+    }
+    if (type === 'surface water') {
+      extentInfoRs.style.display = 'none'
+      extentInfoReservoirs.style.display = 'none'
+      extentInfoSw.style.display = 'block'
+      depthInfo.style.display = 'none'
+      velocityInfo.style.display = 'none'
+      scenarioBarDepth.style.display = 'none'
+      scenarioBarVelocity.style.display = 'none'
+      copyrightBtn.style.top = 'calc(100vh - 115px)'
+      copyrightInfo.style.right = '360px'
+      olZoom[0].style.top = 'calc(100% - 145px)'
+    }
   }
 }
 /* eslint-disable no-unused-vars */
 
 /* eslint-disable no-unused-vars */
 function toggleCopyrightInfo () {
-  const copyrightInfoContainer = document.getElementsByClassName('defra-map-info__container')
+  const copyrightInfoContainer = document.getElementById('copyright-info-container')
   const scenarioBarDepth = document.getElementById('scenario-container-depth')
   const scenarioBarVelocity = document.getElementById('scenario-container-velocity')
-  const extentRadio = document.getElementById('sw-extent-radio')
-  if (!extentRadio.checked) {
-    if (scenarioBarDepth.style.display === 'block') {
-      if (!copyrightInfoContainer[0].classList.contains('showing')) {
-        scenarioBarDepth.style.display = 'none'
-      }
-    }
-    if (scenarioBarDepth.style.display === 'none') {
-      if (copyrightInfoContainer[0].classList.contains('showing')) {
-        scenarioBarDepth.style.display = 'block'
-        scenarioBarVelocity.style.display = 'none'
-      }
-    }
 
-    if (scenarioBarVelocity.style.display === 'block') {
-      if (!copyrightInfoContainer[0].classList.contains('showing')) {
-        scenarioBarVelocity.style.display = 'none'
-      }
-    }
-    if (scenarioBarVelocity.style.display === 'none') {
-      if (copyrightInfoContainer[0].classList.contains('showing')) {
-        scenarioBarVelocity.style.display = 'block'
-        scenarioBarDepth.style.display = 'none'
-      }
-    }
-  }
+  const depthRadio = document.getElementById('sw-depth-radio')
+  const velocityRadio = document.getElementById('sw-velocity-radio')
 
-  if (!copyrightInfoContainer[0].classList.contains('showing')) {
-    copyrightInfoContainer[0].classList.add('showing')
-    copyrightInfoContainer[0].style.display = 'block'
+  if (copyrightInfoContainer.style.display === 'none') {
+    if (scenarioBarDepth.style.display === 'block' || scenarioBarVelocity.style.display === 'block') {
+      scenarioBarDepth.style.display = 'none'
+      scenarioBarVelocity.style.display = 'none'
+    }
+    copyrightInfoContainer.style.display = 'block'
   } else {
-    copyrightInfoContainer[0].classList.remove('showing')
-    copyrightInfoContainer[0].style.display = 'none'
+    if (depthRadio.checked) {
+      scenarioBarDepth.style.display = 'block'
+    }
+    if (velocityRadio.checked) {
+      scenarioBarVelocity.style.display = 'block'
+    }
+    copyrightInfoContainer.style.display = 'none'
   }
 }
 /* eslint-enable no-unused-vars */
@@ -184,10 +214,8 @@ function toggleCopyrightInfo () {
 function toggleAdvancedOptions () {
   const advancedMapOptions = document.getElementsByClassName('advanced-map-option')
   if (!advancedMapOptions[0].classList.contains('showing')) {
-    advancedMapOptions[0].classList.add('showing')
     advancedMapOptions[0].style.display = 'block'
   } else {
-    advancedMapOptions[0].classList.remove('showing')
     advancedMapOptions[0].style.display = 'none'
   }
 }
@@ -218,7 +246,8 @@ function selectedOption () {
   const scenariosRadiosDepth = document.querySelector('input[name="scenarios-depth"]:checked')
   const scenariosRadiosVelocity = document.querySelector('input[name="scenarios-velocity"]:checked')
 
-  const extentRadio = document.getElementById('sw-extent-radio')
+  const extentRadioReservoirs = document.getElementById('reservoirs-radio')
+  const extentRadioSw = document.getElementById('sw-extent-radio')
   const depthRadio = document.getElementById('sw-depth-radio')
   const velocityRadio = document.getElementById('sw-velocity-radio')
 
@@ -226,7 +255,10 @@ function selectedOption () {
   const lowRadioDepth = document.getElementById('risk-radio-low-depth')
   const mediumRadioVelocity = document.getElementById('risk-radio-medium-velocity')
   const lowRadioVelocity = document.getElementById('risk-radio-low-velocity')
-  if (extentRadio.checked) {
+  if (extentRadioSw.checked) {
+    return measurementsRadios.value
+  }
+  if (extentRadioReservoirs.checked) {
     return measurementsRadios.value
   }
   if (depthRadio.checked) {
