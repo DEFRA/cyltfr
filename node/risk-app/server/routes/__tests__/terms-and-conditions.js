3a744ee89ee830f35da5f365fc383eceed672486
Matthew Collins <matthew.collins@defra.gov.uk>
2024-02-29 13:58:40 +0000

    Just one more test, I can stop anytime

---
+++ node/risk-app/server/routes/__tests__/terms-and-conditions.js
@@ -0,0 +1,23 @@
+const STATUS_CODES = require('http2').constants
+const createServer = require('../../../server')
+let server
+
+beforeAll(async () => {
+  server = await createServer()
+  await server.initialize()
+})
+
+afterAll(async () => {
+  await server.stop()
+})
+
+describe('/Terms and Conditions test', () => {
+  it('Assert Terms and Conditions statement page', async () => {
+    const options = {
+      method: 'GET',
+      url: '/terms-and-conditions'
+    }
+    const response = await server.inject(options)
+    expect(response.statusCode).toEqual(STATUS_CODES.HTTP_STATUS_OK) // 200
+  })
+})
