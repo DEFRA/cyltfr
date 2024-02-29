cd33fcc593901383db940b546c1dfab1047c4925
Matthew Collins <matthew.collins@defra.gov.uk>
2024-02-29 15:33:05 +0000

    Move os-maps-proxy test to Jest

--- node/risk-app/server/routes/__tests__/os-maps-proxy.js
+++ node/risk-app/server/routes/__tests__/os-maps-proxy.js
@@ -1,24 +1,21 @@
-const Lab = require('@hapi/lab')
-const Code = require('@hapi/code')
-const createServer = require('../../server')
-const lab = exports.lab = Lab.script()
-const utils = require('../../server/util')
-const { mock } = require('../mock')
-// const { payloadMatchTest } = require('../utils')
+const STATUS_CODES = require('http2').constants
+const createServer = require('../../../server')
+const util = require('../../util')
+let server
 
-lab.experiment('/os-maps-proxy test', () => {
-  let server
+jest.mock('../../util')
 
-  lab.before(async () => {
-    server = await createServer()
-    await server.initialize()
-  })
+beforeAll(async () => {
+  server = await createServer()
+  await server.initialize()
+})
 
-  lab.after(async () => {
-    await server.stop()
-  })
+afterAll(async () => {
+  await server.stop()
+})
 
-  lab.test('/os-maps-proxy standard call', async () => {
+describe('/os-maps-proxy test', () => {
+  it('/os-maps-proxy standard call', async () => {
     const options = {
       method: 'GET',
       url: '/os-maps-proxy?layer=Road_27700&style=default&tilematrixset=EPSG%3A27700&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fpng&TileMatrix=EPSG%3A27700%3A8&TileCol=636&TileRow=1374',
@@ -26,18 +23,12 @@ lab.experiment('/os-maps-proxy test', () => {
 
       }
     }
-    const proxyResponse = () => {
-      return 'Response'
-    }
-
-    const fetchstub = mock.replace(utils, 'get', proxyResponse)
-
+    util.get.mockResolvedValue('Response')
     const response = await server.inject(options)
-    Code.expect(response.statusCode).to.equal(200)
-    fetchstub.revert()
+    expect(response.statusCode).toEqual(STATUS_CODES.HTTP_STATUS_OK) // 200
   })
 
-  lab.test('/os-maps-proxy error call', async () => {
+  it('/os-maps-proxy error call', async () => {
     const options = {
       method: 'GET',
       url: '/os-maps-proxy?layer=Road_27700&style=default&tilematrixset=EPSG%3A27700&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fpng&TileMatrix=EPSG%3A27700%3A8&TileCol=636&TileRow=1374',
@@ -49,20 +40,17 @@ lab.experiment('/os-maps-proxy test', () => {
     let notifyCalled = false
     const newNotify = () => { notifyCalled = true }
     server.methods.notify = newNotify
-    const proxyResponse = () => {
+    util.get.mockImplementation(() => {
       const fakeResponse = { FakeResponse: true }
       throw new Error('Error during call', fakeResponse)
-    }
-    const fetchstub = mock.replace(utils, 'get', proxyResponse)
-
+    })
     const response = await server.inject(options)
-    Code.expect(response.statusCode).to.equal(400)
-    Code.expect(notifyCalled).to.equal(true)
     server.methods.notify = oldNotify
-    fetchstub.revert()
+    expect(response.statusCode).toEqual(STATUS_CODES.HTTP_STATUS_BAD_REQUEST) // 400
+    expect(notifyCalled).toEqual(true)
   })
 
-  lab.test('/os-maps-proxy error call no notify', async () => {
+  it('/os-maps-proxy error call no notify', async () => {
     const options = {
       method: 'GET',
       url: '/os-maps-proxy?layer=Road_27700&style=default&tilematrixset=EPSG%3A27700&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fpng&TileMatrix=EPSG%3A27700%3A8&TileCol=636&TileRow=1374',
@@ -72,15 +60,8 @@ lab.experiment('/os-maps-proxy test', () => {
     }
     const oldNotify = server.methods.notify
     server.methods.notify = null
-    const proxyResponse = () => {
-      const fakeResponse = { FakeResponse: true }
-      throw new Error('Error during call', fakeResponse)
-    }
-    const fetchstub = mock.replace(utils, 'get', proxyResponse)
-
     const response = await server.inject(options)
-    Code.expect(response.statusCode).to.equal(400)
+    expect(response.statusCode).toEqual(STATUS_CODES.HTTP_STATUS_BAD_REQUEST) // 400
     server.methods.notify = oldNotify
-    fetchstub.revert()
   })
 })
