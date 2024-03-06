f346e32cc95d85b6772808aacbb5b178748a246a
Matthew Collins <matthew.collins@defra.gov.uk>
2024-03-06 11:16:28 +0000

    Fix search tests for flood warnings

--- node/risk-app/test/routes/search.js
+++ node/risk-app/test/routes/search.js
@@ -9,6 +9,13 @@ const addressService = require('../../server/services/address')
 const utils = require('../../server/util')
 const captchaCheck = require('../../server/services/captchacheck')
 const { payloadMatchTest } = require('../utils')
+const STATUS_CODES = require('http2').constants
+const floodWarning = {
+  address: 'Bognor Regis, PO22 9HY',
+  floods: [],
+  severity: 2,
+  message: 'There are currently one flood warning and 2 flood alerts in force at this location.'
+}
 
 lab.experiment('search page route', () => {
   let server, cookie, addressStub, warningStub
@@ -20,7 +27,7 @@ lab.experiment('search page route', () => {
     const initial = mockOptions()
 
     const homepageresponse = await server.inject(initial)
-    Code.expect(homepageresponse.statusCode).to.equal(200)
+    Code.expect(homepageresponse.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_OK)
     cookie = homepageresponse.headers['set-cookie'][0].split(';')[0]
   })
 
@@ -40,35 +47,37 @@ lab.experiment('search page route', () => {
     await server.stop()
   })
 
-  lab.test('/search - banner ', async () => {
-    // This doesn't seem to actually test anything TODO: Check this test
-    const { getOptions } = mockSearchOptions('cw8 4bh', cookie)
-    const getResponse = await server.inject(getOptions)
-    Code.expect(getResponse.statusCode).to.equal(200)
-  })
-
   lab.test('/address - No banner warnings', async () => {
-    // This doesn't seem to actually test anything TODO: Check this test
     const { getOptions } = mockSearchOptions('cw8 4bh', cookie)
+    const noFloodWarning = { }
+    const floodServiceStub = mock.replace(floodService, 'findWarnings', mock.makePromise(null, noFloodWarning))
     const getResponse = await server.inject(getOptions)
-    Code.expect(getResponse.statusCode).to.equal(200)
+    Code.expect(getResponse.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_OK)
+    await payloadMatchTest(getResponse.payload, /Warning<\/span>/g, 0)
+    floodServiceStub.revert()
   })
 
   lab.test('/address - flood warnings for unknown address', async () => {
-    // TODO: Return a result with address of "England" to test what happens with whole country results
     const { getOptions } = mockSearchOptions('cw8 4bh', cookie)
+    const noFloodWarning = { ...floodWarning }
+    noFloodWarning.address = 'England'
+    const floodServiceStub = mock.replace(floodService, 'findWarnings', mock.makePromise(null, noFloodWarning))
     const getResponse = await server.inject(getOptions)
-    Code.expect(getResponse.statusCode).to.equal(200)
+    Code.expect(getResponse.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_OK)
+    const re = new RegExp(noFloodWarning.message, 'g')
+    await payloadMatchTest(getResponse.payload, re, 0)
+    floodServiceStub.revert()
   })
 
   lab.test('/search - No warning banner severity', async () => {
-    // This doesn't seem to actually test anything TODO: Check this test
     const { getOptions } = mockSearchOptions('cw8 4bh', cookie)
-    const floodServiceStub = mock.replace(floodService, 'findWarnings', mock.makePromise(null, {
-      message: 'There is currently one flood alert in force at this location.'
-    }))
+    const noFloodWarning = { ...floodWarning }
+    noFloodWarning.severity = 5
+    const floodServiceStub = mock.replace(floodService, 'findWarnings', mock.makePromise(null, noFloodWarning))
     const getResponse = await server.inject(getOptions)
-    Code.expect(getResponse.statusCode).to.equal(200)
+    Code.expect(getResponse.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_OK)
+    const re = new RegExp(noFloodWarning.message, 'g')
+    await payloadMatchTest(getResponse.payload, re, 0)
     floodServiceStub.revert()
   })
 
@@ -78,8 +87,9 @@ lab.experiment('search page route', () => {
     const data = require('../data/banner-1.json')
     const floodServiceStub = mock.replace(floodService, 'findWarnings', mock.makePromise(null, data))
     const getResponse = await server.inject(getOptions)
-    Code.expect(getResponse.statusCode).to.equal(200)
-    await payloadMatchTest(getResponse.payload, /There is currently one flood alert in force at this location/g)
+    Code.expect(getResponse.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_OK)
+    const re = new RegExp(data.message, 'g')
+    await payloadMatchTest(getResponse.payload, re, 1)
     floodServiceStub.revert()
   })
 
@@ -88,8 +98,9 @@ lab.experiment('search page route', () => {
     const data = require('../data/banner-2.json')
     const floodServiceStub = mock.replace(floodService, 'findWarnings', mock.makePromise(null, data))
     const getResponse = await server.inject(getOptions)
-    Code.expect(getResponse.statusCode).to.equal(200)
-    await payloadMatchTest(getResponse.payload, /There is currently one flood alert in force at this location/g)
+    Code.expect(getResponse.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_OK)
+    const re = new RegExp(data.message, 'g')
+    await payloadMatchTest(getResponse.payload, re, 1)
     floodServiceStub.revert()
   })
 
@@ -101,7 +112,7 @@ lab.experiment('search page route', () => {
     const newNotify = () => { notifyCalled = true }
     server.methods.notify = newNotify
     const getResponse = await server.inject(getOptions)
-    Code.expect(getResponse.statusCode).to.equal(200)
+    Code.expect(getResponse.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_OK)
     Code.expect(notifyCalled).to.equal(true)
     server.methods.notify = oldNotify
     floodServiceStub.revert()
@@ -110,13 +121,13 @@ lab.experiment('search page route', () => {
   lab.test('/search', async () => {
     const { getOptions } = mockSearchOptions('cw8 4bh', cookie)
     const getResponse = await server.inject(getOptions)
-    Code.expect(getResponse.statusCode).to.equal(200)
+    Code.expect(getResponse.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_OK)
   })
 
   lab.test('/search - Invalid postcode - fails regexp', async () => {
     const { getOptions } = mockSearchOptions('invalid', cookie)
     const getResponse = await server.inject(getOptions)
-    Code.expect(getResponse.statusCode).to.equal(400)
+    Code.expect(getResponse.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_BAD_REQUEST)
   })
 
   lab.test('/search - Invalid query', async () => {
@@ -127,14 +138,14 @@ lab.experiment('search page route', () => {
     }
     const captchastub = mock.replace(captchaCheck, 'captchaCheck', mock.makePromise(null, mockCaptchaCheck('foo')))
     const response = await server.inject(options)
-    Code.expect(response.statusCode).to.equal(400)
+    Code.expect(response.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_BAD_REQUEST)
     captchastub.revert()
   })
 
   lab.test('/search - Invalid postcode - passes regexp', async () => {
     const { getOptions } = mockSearchOptions('XX11 1XX', cookie)
     const getResponse = await server.inject(getOptions)
-    Code.expect(getResponse.statusCode).to.equal(200)
+    Code.expect(getResponse.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_OK)
   })
 
   lab.test('/search - Address service error', async () => {
@@ -147,7 +158,7 @@ lab.experiment('search page route', () => {
     addressServiceMock.returns(false)
     const captchastub = mock.replace(utils, 'post', mock.makePromise(null, mockCaptchaResponse(true, null)))
     const responseUrl = await server.inject(options)
-    Code.expect(responseUrl.statusCode).to.equal(400)
+    Code.expect(responseUrl.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_BAD_REQUEST)
     captchastub.revert()
   })
 
@@ -155,7 +166,7 @@ lab.experiment('search page route', () => {
     const { getOptions } = mockSearchOptions('cw8 4bh', cookie)
     const customAddressStub = mock.replace(addressService, 'find', mock.makePromise(null, []))
     const getResponse = await server.inject(getOptions)
-    Code.expect(getResponse.statusCode).to.equal(200)
+    Code.expect(getResponse.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_OK)
     customAddressStub.revert()
   })
 
@@ -163,7 +174,7 @@ lab.experiment('search page route', () => {
     const { getOptions } = mockSearchOptions('cw8 4bh', cookie)
     const captchastub = mock.replace(utils, 'post', mock.makePromise(null, mockCaptchaResponse(true, null)))
     const getResponse = await server.inject(getOptions)
-    Code.expect(getResponse.statusCode).to.equal(200)
+    Code.expect(getResponse.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_OK)
     captchastub.revert()
   })
 
@@ -171,7 +182,7 @@ lab.experiment('search page route', () => {
     const { getOptions } = mockSearchOptions('BT11BT', cookie)
 
     const getResponse = await server.inject(getOptions)
-    Code.expect(getResponse.statusCode).to.equal(302)
+    Code.expect(getResponse.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_FOUND)
     Code.expect(getResponse.headers.location).to.include('/england-only')
   })
 
@@ -186,7 +197,7 @@ lab.experiment('search page route', () => {
     }
     const captchastub = mock.replace(utils, 'post', mock.makePromise(null, mockCaptchaResponse(true, null)))
     const response = await server.inject(options)
-    Code.expect(response.statusCode).to.equal(302)
+    Code.expect(response.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_FOUND)
     captchastub.revert()
   })
 
@@ -197,21 +208,21 @@ lab.experiment('search page route', () => {
     }
 
     const response = await server.inject(options)
-    Code.expect(response.statusCode).to.equal(400)
+    Code.expect(response.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_BAD_REQUEST)
   })
 
   // This test fails if the Captcha is not enabled. This needs fixing.
   // lab.test('should redirect user to postcode page if captcha times out or not found', async () => {
   //   const captchastub = mock.replace(utils, 'post', mock.makePromise(null, mockCaptchaResponse(false, null)))
   //   const responseUrl = await server.inject(mockSearchOptions('cw8 4bh').getOptions)
-  //   Code.expect(responseUrl.statusCode).to.equal(302)
+  //   Code.expect(responseUrl.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_FOUND)
   //   captchastub.revert()
   // })
 
   lab.test('should get search page with postcode if already queried and captcha not expired', async () => {
     const captchastub = mock.replace(utils, 'post', mock.makePromise(null, mockCaptchaResponse(true, null)))
     const responseUrl = await server.inject(mockSearchOptions('cw8 4bh', cookie).getOptions)
-    Code.expect(responseUrl.statusCode).to.equal(200)
+    Code.expect(responseUrl.statusCode).to.equal(STATUS_CODES.HTTP_STATUS_OK)
     captchastub.revert()
   })
 })
