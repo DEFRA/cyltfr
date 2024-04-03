8cadf414ea44f49484b283dc2180942918d339ea
Matthew Collins <matthew.collins@defra.gov.uk>
2024-04-03 14:46:59 +0100

    OMG!

--- node/risk-app/server/src/js/map.js
+++ node/risk-app/server/src/js/map.js
@@ -108,7 +108,7 @@ function mapControls () {
     const retval = document.createElementNS('http://www.w3.org/2000/svg', tagName)
     if (props) {
       for (const name in props) {
-        if (Object.prototype.hasOwnProperty.call(props, name)) {
+        if (Object.hasOwn(props, name)) {
           retval.setAttribute(name, props[name])
         }
       }
