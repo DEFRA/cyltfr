5218065d5dedfa56de3c1f7b2ae1a14365b6b96b
Matthew-Collins-Defra <48916014+Matthew-Collins-Defra@users.noreply.github.com>
2024-04-03 15:14:11 +0100

    Openlayers update (#247)
    * Openlayers update
    
    https://eaflood.atlassian.net/browse/LTFRI-1109

---
+++ node/risk-app/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
+  devtool: 'source-map',
+  entry: {
+    map: './server/src/js/map.js'
+  },
+  node: {
+    global: true
+  },
+  output: {
+    path: path.resolve(__dirname, 'server/public/build/js'),
+    library: 'maps'
+  }
+}
