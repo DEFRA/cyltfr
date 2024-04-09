af709eb097ad900f513d4dd7940ec692d49b93a7
Matthew Collins <matthew.collins@defra.gov.uk>
2024-04-09 15:02:38 +0100

    Remove lab packages

--- node/risk-app/test/utils.js
+++
@@ -1,7 +0,0 @@
-const Code = require('@hapi/code')
-module.exports = {
-  payloadMatchTest: async (payload, regex, expectedMatchCount = 1) => {
-    const errorMatch = payload.match(regex) || []
-    Code.expect(errorMatch.length, `\nFailed Regex match: ${regex}\n`).to.equal(expectedMatchCount)
-  }
-}
