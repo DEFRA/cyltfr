4319662676a0feb1a0ce04b64b9553812a0f1e7a
Matthew Collins <matthew.collins@defra.gov.uk>
2024-04-09 14:26:27 +0100

    Remove unused function

--- node/risk-app/server/services/__mocks__/address.js
+++ node/risk-app/server/services/__mocks__/address.js
@@ -1,17 +1,9 @@
 const addressService = jest.createMockFromModule('../address')
-const findById = require('./find-by-id')
 const findByPostcode = require('./find')
 
 /**
  * Override the real functions with mock implementations
  */
-addressService.findById.mockImplementation((id) => {
-  const result = findById[id]
-  return result
-    ? Promise.resolve(result)
-    : Promise.reject(new Error(`findById mock not found for id [${id}]`))
-})
-
 addressService.find.mockImplementation((postcode) => {
   const result = findByPostcode[postcode.toUpperCase().replace(' ', '')]
   return result
