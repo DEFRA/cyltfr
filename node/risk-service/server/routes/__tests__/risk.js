450d6e22eb9a2206e7eb7a3975e3483b1ffe728e
Matthew Collins <matthew.collins@defra.gov.uk>
2024-04-10 12:23:19 +0100

    Move test data to extrnal module

--- node/risk-service/server/routes/__tests__/risk.js
+++ node/risk-service/server/routes/__tests__/risk.js
@@ -1,6 +1,7 @@
 const STATUS_CODES = require('http2').constants
 const createServer = require('../../../server')
 const db = require('../../db')
+const testData = require('./__test_data__/')
 const options = {
   method: 'GET',
   url: '/floodrisk/391416/102196/20'
@@ -20,21 +21,8 @@ describe('Unit tests - /floodrisk', () => {
   })
 
   test('Normal get returns the payload', async () => {
-    db._queryResult([
-      {
-        calculate_flood_risk: {
-          in_england: true,
-          flood_alert_area: null,
-          flood_warning_area: null,
-          rofrs_risk: null,
-          surface_water_risk: 'Very Low',
-          reservoir_risk: null,
-          surface_water_suitability: 'County to Town',
-          lead_local_flood_authority: 'Cheshire West and Chester',
-          extra_info: null
-        }
-      }
-    ])
+    const inputData = testData.getEmptyData()
+    db._queryResult(inputData)
 
     const response = await server.inject(options)
     expect(response.statusCode).toEqual(STATUS_CODES.HTTP_STATUS_OK)
@@ -54,39 +42,8 @@ describe('Unit tests - /floodrisk', () => {
   })
 
   test('/floodrisk/{x}/{y}/{radius} - Valid db result', async () => {
-    db._queryResult([
-      {
-        calculate_flood_risk: {
-          in_england: true,
-          flood_alert_area: [
-            '033WAF204'
-          ],
-          flood_warning_area: [
-            '033FWF3AVON013'
-          ],
-          rofrs_risk: {
-            prob_4band: 'Medium',
-            suitability: 'Street to Parcels of land',
-            risk_for_insurance_sop: 'Yes'
-          },
-          surface_water_risk: 'High',
-          reservoir_risk: [
-            {
-              resname: 'Draycote Water',
-              risk_desig: 'High Risk',
-              location: '445110, 270060',
-              ut_company: 'Severn Trent Water Authority',
-              ea_area: 'Environment Agency - Staffordshire, Warwickshire and West Midlands',
-              llfa_name: 'Warwickshire',
-              comments: 'If you have questions about local emergency plans for this reservoir you should contact the named Local Authority'
-            }
-          ],
-          surface_water_suitability: 'County to Town',
-          lead_local_flood_authority: 'Warwickshire',
-          extra_info: null
-        }
-      }
-    ])
+    const inputData = testData.getValidData()
+    db._queryResult(inputData)
 
     const response = await server.inject(options)
     expect(response.statusCode).toEqual(STATUS_CODES.HTTP_STATUS_OK)
@@ -100,78 +57,20 @@ describe('Unit tests - /floodrisk', () => {
   })
 
   test('/floodrisk/{x}/{y}/{radius} - Groundwater alert result', async () => {
-    db._queryResult([
-      {
-        calculate_flood_risk: {
-          in_england: true,
-          flood_alert_area: [
-            '033WAG204'
-          ],
-          flood_warning_area: [
-            '033FWF3AVON013'
-          ],
-          rofrs_risk: {
-            prob_4band: 'Medium',
-            suitability: 'Street to Parcels of land',
-            risk_for_insurance_sop: 'Yes'
-          },
-          surface_water_risk: 'High',
-          reservoir_risk: [
-            {
-              resname: 'Draycote Water',
-              risk_desig: 'High Risk',
-              location: '445110, 270060',
-              ut_company: 'Severn Trent Water Authority',
-              ea_area: 'Environment Agency - Staffordshire, Warwickshire and West Midlands',
-              llfa_name: 'Warwickshire',
-              comments: 'If you have questions about local emergency plans for this reservoir you should contact the named Local Authority'
-            }
-          ],
-          surface_water_suitability: 'County to Town',
-          lead_local_flood_authority: 'Warwickshire',
-          extra_info: null
-        }
-      }
-    ])
+    const inputData = testData.getValidData()
+    // Clear out the warning data
+    inputData[0].calculate_flood_risk.flood_warning_area = null
+    db._queryResult(inputData)
 
     const response = await server.inject(options)
     expect(response.statusCode).toEqual(STATUS_CODES.HTTP_STATUS_OK)
   })
 
   test('/floodrisk/{x}/{y}/{radius} - Groundwater warning result', async () => {
-    db._queryResult([
-      {
-        calculate_flood_risk: {
-          in_england: true,
-          flood_alert_area: [
-            '033WAF204'
-          ],
-          flood_warning_area: [
-            '033FWG3AVON013'
-          ],
-          rofrs_risk: {
-            prob_4band: 'Medium',
-            suitability: 'Street to Parcels of land',
-            risk_for_insurance_sop: 'Yes'
-          },
-          surface_water_risk: 'High',
-          reservoir_risk: [
-            {
-              resname: 'Draycote Water',
-              risk_desig: 'High Risk',
-              location: '445110, 270060',
-              ut_company: 'Severn Trent Water Authority',
-              ea_area: 'Environment Agency - Staffordshire, Warwickshire and West Midlands',
-              llfa_name: 'Warwickshire',
-              comments: 'If you have questions about local emergency plans for this reservoir you should contact the named Local Authority'
-            }
-          ],
-          surface_water_suitability: 'County to Town',
-          lead_local_flood_authority: 'Warwickshire',
-          extra_info: null
-        }
-      }
-    ])
+    const inputData = testData.getValidData()
+    // Clear out the alert data
+    inputData[0].calculate_flood_risk.flood_alert_area = null
+    db._queryResult(inputData)
 
     const response = await server.inject(options)
     expect(response.statusCode).toEqual(STATUS_CODES.HTTP_STATUS_OK)
@@ -193,107 +92,19 @@ describe('Unit tests - /floodrisk', () => {
   })
 
   test('/floodrisk/{x}/{y}/{radius} - Extra info error', async () => {
-    db._queryResult([
-      {
-        calculate_flood_risk: {
-          in_england: true,
-          flood_alert_area: [
-            '033WAF204'
-          ],
-          flood_warning_area: [
-            '033FWG3AVON013'
-          ],
-          rofrs_risk: {
-            prob_4band: 'Medium',
-            suitability: 'Street to Parcels of land',
-            risk_for_insurance_sop: 'Yes'
-          },
-          surface_water_risk: 'High',
-          reservoir_risk: [
-            {
-              resname: 'Draycote Water',
-              risk_desig: 'High Risk',
-              location: '445110, 270060',
-              ut_company: 'Severn Trent Water Authority',
-              ea_area: 'Environment Agency - Staffordshire, Warwickshire and West Midlands',
-              llfa_name: 'Warwickshire',
-              comments: 'If you have questions about local emergency plans for this reservoir you should contact the named Local Authority'
-            }
-          ],
-          surface_water_suitability: 'County to Town',
-          lead_local_flood_authority: 'Warwickshire',
-          extra_info: 'Error'
-        }
-      }
-    ])
+    const inputData = testData.getValidData()
+    inputData[0].calculate_flood_risk.extra_info = 'Error'
+    db._queryResult(inputData)
 
     const response = await server.inject(options)
     expect(response.statusCode).toEqual(STATUS_CODES.HTTP_STATUS_OK)
   })
 
   test('/floodrisk/{x}/{y}/{radius} - Extra info result', async () => {
-    db._queryResult([
-      {
-        calculate_flood_risk: {
-          in_england: true,
-          flood_alert_area: [
-            '033WAF204'
-          ],
-          flood_warning_area: [
-            '033FWG3AVON013'
-          ],
-          rofrs_risk: {
-            prob_4band: 'Medium',
-            suitability: 'Street to Parcels of land',
-            risk_for_insurance_sop: 'Yes'
-          },
-          surface_water_risk: 'High',
-          reservoir_risk: [
-            {
-              resname: 'Draycote Water',
-              risk_desig: 'High Risk',
-              location: '445110, 270060',
-              ut_company: 'Severn Trent Water Authority',
-              ea_area: 'Environment Agency - Staffordshire, Warwickshire and West Midlands',
-              llfa_name: 'Warwickshire',
-              comments: 'If you have questions about local emergency plans for this reservoir you should contact the named Local Authority'
-            }
-          ],
-          surface_water_suitability: 'County to Town',
-          lead_local_flood_authority: 'Warwickshire',
-          extra_info: [{
-            info: '',
-            apply: 'holding',
-            riskoverride: 'Low'
-          },
-          {
-            info: '',
-            apply: 'holding',
-            riskoverride: 'Do not override'
-          },
-          {
-            info: 'There are improvements to the flood defences in this area, we expect the flood liklihood in this area to change on 1 April 2020',
-            apply: 'holding',
-            riskoverride: 'Do not override'
-          },
-          {
-            info: 'Some improvements to the flood defences in this area, we expect the flood liklihood in this area to change on 1 April 2020',
-            apply: 'holding',
-            riskoverride: 'Do not override'
-          },
-          {
-            info: 'Proposed schemes',
-            apply: 'llfa',
-            riskoverride: null
-          },
-          {
-            info: 'Flood action plan',
-            apply: 'llfa',
-            riskoverride: 'Do not override'
-          }]
-        }
-      }
-    ])
+    const inputData = testData.getValidData()
+    // The extra info contains a surface water override that will change the High to Low
+    inputData[0].calculate_flood_risk.extra_info = testData.getExtraInfo()
+    db._queryResult(inputData)
 
     const response = await server.inject(options)
     expect(response.statusCode).toEqual(STATUS_CODES.HTTP_STATUS_OK)
