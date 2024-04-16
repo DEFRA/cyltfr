d4e752f6abc1b10a0e5fbf83d2dd5d17d16324b5
Matthew Collins <matthew.collins@defra.gov.uk>
2024-04-16 16:06:35 +0100

    Another go at code complexity

--- node/risk-admin/server/public/static/js/partials/sharedFunctions.js
+++ node/risk-admin/server/public/static/js/partials/sharedFunctions.js
@@ -28,61 +28,56 @@ const sharedFunctions = {
     const riskTypes = document.getElementsByClassName(`risk-type-${index}`)
     const textCommentRadios = document.getElementsByClassName(`textComment_radio_${index}`)
 
-    const checkRiskOptions = () => {
-      const riskRadios = document.getElementsByClassName(`risk-option_${index}`)
-      for (const radio of riskRadios) {
-        if (radio.value === selectedRadio[index]) {
-          riskOptionRadios.style.display = 'block'
-          overrideRadio.checked = true
-          radio.checked = true
-        }
-      }
-    }
-
-    const textArea = document.getElementById(`text_area_${index}`)
-
-    if (isHoldingComment) {
-      checkRiskOptions()
-
-      for (const typeRadio of riskTypes) {
-        typeRadio.checked = (typeRadio.value === riskType[index])
-      }
-
-      for (const commentRadio of textCommentRadios) {
-        if (commentRadio.value === textCommentRadio[index]) {
-          commentRadio.checked = true
-        }
-        if (textCommentRadio[index] === 'No') {
-          textArea.style.display = 'none'
-        }
-      }
-
-      overrideRadioSection.style.display = rsRadio.checked ? 'none' : 'block'
-
-      if (overrideRadio.checked) {
-        riskOptionRadios.style.display = 'block'
-      }
-
-      overrideRadio.addEventListener('click', function () {
-        noOverrideRadio.checked = false
-        riskOptionRadios.style.display = 'block'
-      })
-      noOverrideRadio.addEventListener('click', function () {
-        overrideRadio.checked = false
-        riskOptionRadios.style.display = 'none'
-      })
-      riskTypeRadios.addEventListener('change', function () {
-        overrideRadioSection.style.display = rsRadio.checked ? 'none' : 'block'
-      })
-      addCommentRadios.addEventListener('change', function () {
-        textArea.style.display = noHoldingCommentTextRadio.checked ? 'none' : 'block'
-      })
-    } else {
+    if (!isHoldingComment) {
       const riskReportRadios = document.getElementsByClassName(`risk-report_${index}`)
       for (const radio of riskReportRadios) {
         radio.checked = (radio.value === selectedRadio[index])
       }
+      return
     }
+
+    const riskRadios = document.getElementsByClassName(`risk-option_${index}`)
+    for (const radio of riskRadios) {
+      if (radio.value === selectedRadio[index]) {
+        riskOptionRadios.style.display = 'block'
+        overrideRadio.checked = true
+        radio.checked = true
+      }
+    }
+
+    for (const typeRadio of riskTypes) {
+      typeRadio.checked = (typeRadio.value === riskType[index])
+    }
+
+    for (const commentRadio of textCommentRadios) {
+      if (commentRadio.value === textCommentRadio[index]) {
+        commentRadio.checked = true
+      }
+    }
+
+    overrideRadioSection.style.display = rsRadio.checked ? 'none' : 'block'
+
+    if (overrideRadio.checked) {
+      riskOptionRadios.style.display = 'block'
+    }
+
+    overrideRadio.addEventListener('click', function () {
+      noOverrideRadio.checked = false
+      riskOptionRadios.style.display = 'block'
+    })
+    noOverrideRadio.addEventListener('click', function () {
+      overrideRadio.checked = false
+      riskOptionRadios.style.display = 'none'
+    })
+    riskTypeRadios.addEventListener('change', function () {
+      overrideRadioSection.style.display = rsRadio.checked ? 'none' : 'block'
+    })
+
+    const textArea = document.getElementById(`text_area_${index}`)
+    textArea.style.display = noHoldingCommentTextRadio.checked ? 'none' : 'block'
+    addCommentRadios.addEventListener('change', function () {
+      textArea.style.display = noHoldingCommentTextRadio.checked ? 'none' : 'block'
+    })
   }
 
 }
