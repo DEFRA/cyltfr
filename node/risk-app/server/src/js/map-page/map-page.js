694c03ffe3c9b8529c88d7ee57357d502ff2374b
michael steinacher <steinacherm@hotmail.co.uk>
2024-04-18 10:36:16 +0100

    Fixed regex exp according to sonarcloud suggestions.

--- node/risk-app/server/src/js/map-page/map-page.js
+++ node/risk-app/server/src/js/map-page/map-page.js
@@ -51,8 +51,8 @@ function mapPage () {
   getInitialKeyOptions()
 
   function getParameterByName (name) {
-    name = name.replace(/[[]/, '\\[').replace(/[\]]/, '\\]')
-    const regex = new RegExp('[\\?&]' + name + '=([^&#]*)')
+    name = name.replace(/[[]/, '\\[').replace(/\]/, '\\]')
+    const regex = new RegExp(`[\\?&]${name}=([^&#]*)`)
     const results = regex.exec(window.location.search)
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '))
   }
