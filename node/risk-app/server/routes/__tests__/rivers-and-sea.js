30e8b04c35666751d5c9e728ddee6a58c103f377
michael steinacher <steinacherm@hotmail.co.uk>
2024-05-20 16:39:16 +0100

    Updated tests to cover redirect with no address and 200ok for happy path

--- node/risk-app/server/routes/__tests__/rivers-and-sea.js
+++ node/risk-app/server/routes/__tests__/rivers-and-sea.js
@@ -1,9 +1,7 @@
 const STATUS_CODES = require('http2').constants
 const createServer = require('../../../server')
-const riskService = require('../../services/risk')
-const mockYar = require('@hapi/yar')
 
-console.log(mockYar.plugin.register)
+let mockAddress
 
 jest.mock('@hapi/yar', () => ({
   plugin: {
@@ -11,14 +9,16 @@ jest.mock('@hapi/yar', () => ({
     register: jest.fn(async (server, _options) => {
       server.decorate('request', 'yar', {
         set: jest.fn(),
-        get: jest.fn()
+        get: jest.fn(() => mockAddress)
       })
     })
   }
 }))
 
 jest.mock('../../services/risk', () => ({
-  getByCoordinates: jest.fn()
+  getByCoordinates: jest.fn().mockResolvedValue({
+    riverAndSeaRisk: { probabilityForBand: 'high' }
+  })
 }))
 
 describe('GET /rivers-and-sea', () => {
@@ -37,54 +37,31 @@ describe('GET /rivers-and-sea', () => {
     jest.clearAllMocks()
   })
 
-  it('returns view with model if risk data is retrieved successfully', async () => {
-    const mockRequest = {
-      method: 'GET',
-      url: '/rivers-and-sea'
-    }
-
-    mockYar.plugin.register.mockImplementationOnce(() => {
-      return { address: { x: 123, y: 123 } }
-    })
-
-    console.log(mockYar)
-
-    const mockResponse = { riverAndSeaRisk: { probabilityForBand: 'high' } }
-    riskService.getByCoordinates.mockResolvedValue(mockResponse)
-    const response = await server.inject(mockRequest)
-    const { payload } = response
-
-    expect(response.statusCode).toEqual(STATUS_CODES.HTTP_STATUS_OK)
-    expect(payload).toMatch(/Rivers and the sea: understand your flood risk/g)
-  })
-
   it('redirects to postcode page if user does not have an address set in session', async () => {
+    mockAddress = undefined
+
     const mockRequest = {
       method: 'GET',
       url: '/rivers-and-sea'
     }
+
     const response = await server.inject(mockRequest)
 
     expect(response.statusCode).toEqual(STATUS_CODES.HTTP_STATUS_FOUND)
     expect(response.headers.location).toBe('/postcode')
   })
 
-  // it('returns 400 if there is an error retrieving risk data', async () => {
-  //   const address` = { x: 1, y: 2 }
+  it('returns 200 OK and renders view if user has an address set in session', async () => {
+    mockAddress = { x: 123, y: 123 }
 
-  //   riskService.getByCoordinates.mockRejectedValue(new Error('test error'))
+    const mockRequest = {
+      method: 'GET',
+      url: '/rivers-and-sea'
+    }
 
-  //   const request = {
-  //     method: 'GET',
-  //     url: '/rivers-and-sea',
-  //     yar: {
-  //       get: jest.fn().mockReturnValue(address)
-  //     }
-  //   }
+    const response = await server.inject(mockRequest)
 
-  //   const response = await server.inject(request)
-
-  //   expect(response.statusCode).toBe(400)
-  //   expect(response.result.message).toBe(errors.riskProfile.message)
-  // })
+    expect(response.statusCode).toEqual(STATUS_CODES.HTTP_STATUS_OK)
+    expect(response.result).toContain('rivers-and-sea') 
+  })
 })
