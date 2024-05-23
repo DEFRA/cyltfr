4984f8e3eef6ed05a36c9eb7ec0996841aed37d4
Matthew Collins <matthew.collins@defra.gov.uk>
2024-05-23 14:33:36 +0100

    Add new page

---
+++ node/risk-app/server/models/surface-water.js
@@ -0,0 +1,9 @@
+function SurfaceWaterViewModel (riskProbability, address, llfa, backLink) {
+  this.easting = address.x
+  this.northing = address.y
+  this.backLink = backLink
+  this.riskProbability = riskProbability
+  this.llfa = llfa
+}
+
+module.exports = SurfaceWaterViewModel
