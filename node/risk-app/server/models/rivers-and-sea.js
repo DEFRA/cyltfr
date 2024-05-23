fed8526448d3ffdbd6d62e69b504dae7a11386bf
michael steinacher <steinacherm@hotmail.co.uk>
2024-05-23 13:03:22 +0100

    Changed riskProbability colours to custom scss colours.

--- node/risk-app/server/models/rivers-and-sea.js
+++ node/risk-app/server/models/rivers-and-sea.js
@@ -1,21 +1,8 @@
 function RiversAndSeaViewModel (riskProbability, address, backLink) {
-  let riskColour
-
-  if (riskProbability === 'High') {
-    riskColour = 'red'
-  } else if (riskProbability === 'Medium') {
-    riskColour = 'orange'
-  } else if (riskProbability === 'Low') {
-    riskColour = 'yellow'
-  } else {
-    riskColour = ''
-  }
-
   this.easting = address.x
   this.northing = address.y
   this.backLink = backLink
   this.riskProbability = riskProbability
-  this.riskColour = riskColour
 }
 
 module.exports = RiversAndSeaViewModel
