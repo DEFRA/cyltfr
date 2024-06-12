76152416c47cfb596aad22e860b2ee1271c1a22b
JoshuaForrest <josh.forrest@aol.com>
2024-06-12 12:11:36 +0100

    Added floodService to server-method.js

--- node/risk-app/server/server-methods/server-methods.js
+++ node/risk-app/server/server-methods/server-methods.js
@@ -1,5 +1,6 @@
 const { find } = require('../services/address')
 const { getByCoordinates } = require('../services/risk')
+const { findWarnings } = require('../services/flood')
 const config = require('../config')
 
 const cacheEnabled = config.cacheEnabled
@@ -21,6 +22,19 @@ const serverMethods = [
   {
     name: 'riskService',
     method: getByCoordinates,
+    options: cacheEnabled
+      ? {
+          cache: {
+            cache: 'server_cache',
+            expiresIn: 100 * 1000,
+            generateTimeout: 20000
+          }
+        }
+      : undefined
+  },
+  {
+    name: 'floodService',
+    method: findWarnings,
     options: cacheEnabled
       ? {
           cache: {
